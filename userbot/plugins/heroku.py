5ae517178b0bda5a434a0908208788a70ce0466a
StarkGang <StarkGangZ@gmail.com>
2020-08-27 10:19:52 +0530

    Update heroku.py

--- userbot/plugins/heroku.py
+++ userbot/plugins/heroku.py
@@ -170,7 +170,7 @@ async def _(dyno):
              Heroku = heroku3.from_key(Var.HEROKU_API_KEY)                         
              app = Heroku.app(Var.HEROKU_APP_NAME)
         except:
-  	       return await dyno.reply(" Please make sure your Heroku API Key, Your App name are configured correctly in the heroku var please check https://t.me/IndianBot_Official/55?single")
+  	       return await dyno.reply(" Please make sure your Heroku API Key, Your App name are configured correctly in the heroku var !")
         await dyno.edit("Downloading Logs..")
         with open('logs.txt', 'w') as log:
             log.write(app.get_log())
