833c1f944b5d1dde710a916b36d5e365d73ba7d2
StarkGang <StarkGangZ@gmail.com>
2020-08-29 16:48:10 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,17 +1,3 @@
-#   Copyright 2019 - 2020 DarkPrinc3
-
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-
-#       http://www.apache.org/licenses/LICENSE-2.0
-
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
 from userbot import bot
 from sys import argv
 import sys
@@ -74,7 +60,7 @@ for name in files:
 
 import userbot._core
 
-print("Yay your userbot is officially working. Ja gaand mara")
+print("Friday Has Been Installed ! Thank You For Trying Friday")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
