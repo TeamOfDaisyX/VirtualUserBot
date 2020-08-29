5860c415c723e072702f0c5f58115390a688c2dc
StarkGang <StarkGangZ@gmail.com>
2020-08-29 22:06:49 +0530

    Test

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -36,7 +36,7 @@ async def gen_chlog(repo, diff):
 
 async def print_changelogs(event, ac_br, changelog):
     changelog_str = (
-        f"**New UPDATE available for [{ac_br}]:\n\nCHANGELOG:**\n{changelog}"
+        f"**New UPDATE available for {ac_br}:\n\nCHANGELOG:**\n{changelog}"
     )
     if len(changelog_str) > 4096:
         await event.edit("`Changelog is too big, view the file to see it.`")
