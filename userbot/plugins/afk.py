435877bd5fe0700f2137a5f49b9b35bff1c2baa5
StarkGang <StarkGangZ@gmail.com>
2020-09-01 07:34:08 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -23,13 +23,13 @@ async def set_not_afk(event):
     if ".afk" not in current_message and "yes" in USER_AFK:  # pylint:disable=E0602
         try:
             await borg.send_message(  # pylint:disable=E0602
-                Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+                Config.PRIVATE_GROUP_ID,  # pylint:disable=E0602
                 "#AfkLogger My Boss Went Afk"
             )
         except Exception as e:  # pylint:disable=C0103,W0703
             await borg.send_message(  # pylint:disable=E0602
                 event.chat_id,
-                "Please set `PRIVATE_GROUP_BOT_API_ID` " + \
+                "Please set `PRIVATE_GROUP_ID` " + \
                 "for the proper functioning of afk functionality " + \
                 "in @FridayOT\n\n `{}`".format(str(e)),
                 reply_to=event.message.id,
@@ -68,7 +68,7 @@ async def _(event):
         await event.delete()
         try:
             await borg.send_message(  # pylint:disable=E0602
-                Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+                Config.PRIVATE_GROUP_ID,  # pylint:disable=E0602
                 f"#AfkLogger Reason : {reason}"
             )
         except Exception as e:  # pylint:disable=C0103,W0703
