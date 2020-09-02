789dfa11d26c49e310af23756a18043c750e603b
StarkGang <StarkGangZ@gmail.com>
2020-09-02 20:31:31 +0530

    Fix

--- heroku_config.py
+++ heroku_config.py
@@ -34,13 +34,7 @@ class Var(object):
         t_file = open(TEMP_DOWNLOAD_DIRECTORY+"auth_token.txt","w")
         t_file.write(AUTH_TOKEN_DATA)
         t_file.close()
-    PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", None)
-    if PRIVATE_GROUP_ID != None:
-        try:
-            PRIVATE_GROUP_ID = int(PRIVATE_GROUP_ID)
-        except ValueError:
-            raise ValueError("Invalid Private Group ID. Make sure your ID is starts with -100 and make sure that it is only numbers.")
-
+    PRIVATE_GROUP_ID = int(os.environ.get("PLUGIN_CHANNEL", None))
 class Development(Var):
     LOGGER = True
     # Here for later purposes
