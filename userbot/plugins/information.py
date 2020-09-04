dc8a29acbc46e4c228b9272ca066dcdb44817ec0
StarkGang <StarkGangZ@gmail.com>
2020-09-04 15:53:15 +0530

    Update information.py

--- userbot/plugins/information.py
+++ userbot/plugins/information.py
@@ -6,7 +6,7 @@ from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_edit
 
 
 @borg.on(admin_cmd("info ?(.*)"))
