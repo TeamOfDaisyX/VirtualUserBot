31a746482a1b1abaadd192685d8614d4b612f21d
StarkGang <StarkGangZ@gmail.com>
2020-09-07 21:05:22 +0530

    Sudo xD

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -5,15 +5,17 @@
 .bye
 """
 from telethon.tl.functions.channels import LeaveChannelRequest
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
 import time
 
 @borg.on(admin_cmd("bye", outgoing=True))
+@borg.on(sudo_cmd("bye", allow_sudo=True))
 async def leave(e):
+    starkgang = await edit_or_reply(e, "Bye Kek")
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("`I am leaving this chat.....!`")
+        await starkgang.edit("`I am leaving this chat.....!`")
         time.sleep(3)
         if '-' in str(e.chat_id):
             await borg(LeaveChannelRequest(e.chat_id))
         else:
-            await e.edit('`Sir This is Not A Chat`')
+            await starkgang.edit('`Sir This is Not A Chat`')
