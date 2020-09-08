beb9f870844285bd76e314321e1b73050bc96629
StarkGang <StarkGangZ@gmail.com>
2020-09-08 21:22:04 +0530

    Update get_admin.py

--- userbot/plugins/get_admin.py
+++ userbot/plugins/get_admin.py
@@ -6,7 +6,7 @@ from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 
 @borg.on(admin_cmd("get_ad?(m)in ?(.*)"))
-@borg.on(admin_cmd("get_ad?(m)in ?(.*)", allow_sudo=True))
+@borg.on(sudo_cmd("get_ad?(m)in ?(.*)", allow_sudo=True))
 async def _(event):
     admeme = await edit_or_reply(event, "Processing")
     if event.fwd_from:
