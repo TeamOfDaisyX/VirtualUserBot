a80d328f46be6acb7ff3bc08f36e81c55cfa7b2b
StarkGang <StarkGangZ@gmail.com>
2020-09-08 22:48:14 +0530

    Update frybot.py

--- userbot/plugins/frybot.py
+++ userbot/plugins/frybot.py
@@ -3,7 +3,7 @@ import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 @borg.on(admin_cmd("frybot ?(.*)"))
 async def _(event):\
