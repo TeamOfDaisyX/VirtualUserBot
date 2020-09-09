3db9c1bb381e06f5e42615a6a451e99c42c52001
StarkGang <StarkGangZ@gmail.com>
2020-09-09 11:40:22 +0530

    Update inline_bot.py

--- userbot/plugins/inline_bot.py
+++ userbot/plugins/inline_bot.py
@@ -16,7 +16,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         builder = event.builder
         result = None
         query = event.text
-        if event.query.user_id == bot.uid and query.startswith("**I Am Alive"):
+        if event.query.user_id == bot.uid and query.startswith("__**Test"):
             buttons = [(custom.Button.inline("Stats", data="statstext"),
                         Button.url("License" , "https://github.com/StarkGang/FridayUserbot/blob/master/LICENSE"),
                         Button.url("Deploy Friday" , "github.com/Starkgang/FridayUserbot"))]
