bc111f8ad1273595fb7c35e3cf114a7f12e77331
StarkGang <StarkGangZ@gmail.com>
2020-09-10 21:32:50 +0530

    Update gDrive.py

--- userbot/plugins/gDrive.py
+++ userbot/plugins/gDrive.py
@@ -2,7 +2,6 @@
 # @Mrconfused
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-
 import asyncio
 import math
 import os
