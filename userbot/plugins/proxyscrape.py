3f910e7466e4f72916f0cdf8bbe1722dbab4483d
StarkGang <StarkGangZ@gmail.com>
2020-09-13 09:33:44 +0530

    Update proxyscrape.py

--- userbot/plugins/proxyscrape.py
+++ userbot/plugins/proxyscrape.py
@@ -15,6 +15,7 @@ STARK_SOCKS4 = "https://api.proxyscrape.com/?request=getproxies&proxytype=socks4
 SOCKS4_TXT = ("**Proxy Info** \nType: __SOCKS4__ \nTimeOut: __10000__ \nCountry: __All__ \nSsl: __Only For Http Proxy__ \nAnonymity: __Only For Http__ \n[Click Here To View Or Download File Manually](https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=10000&country=all) \nUploaded By [Friday](https://github.com/starkgang/FridayUserBot) \n**Here Is Your Proxy** 👇")
 STARK_SOCKS5 = "https://api.proxyscrape.com/?request=getproxies&proxytype=socks5&timeout=10000&country=all"
 SOCKS5_TXT = ("**Proxy Info** \nType: __SOCKS4__ \nTimeOut: __10000__ \nCountry: __All__ \nSsl: __Only For Http Proxy__ \nAnonymity: __Only For Http__ \n[Click Here To View Or Download File Manually](https://api.proxyscrape.com/?request=getproxies&proxytype=socks5&timeout=10000&country=all) \nUploaded By [Friday](https://github.com/starkgang/FridayUserBot) \n**Here Is Your Proxy** 👇")
+sedpng = "https://soon.proxyscrape.com/asset/img/service/downloadicon.svg"
 
 @borg.on(admin_cmd(pattern="proxyhttp")) 
 async def starkxD(event): 
@@ -23,8 +24,13 @@ async def starkxD(event):
     downloaded_file_name = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY , file_name)
     downloader = SmartDL( f"{STARK_HTTP}" , downloaded_file_name, progress_bar=False)
     downloader.start(blocking=False) 
-    await borg.send_message(event.chat_id , HTTP_TXT)
-    await event.client.send_file(event.chat_id , downloaded_file_name) 
+    await event.client.send_file(
+        event.chat_id
+        downloaded_file_name
+        force_document=True,
+        thumb=sedpng,
+        caption=HTTP_TXT,
+        reply_to=message_id) 
     
 @borg.on(admin_cmd(pattern="proxysocks4")) 
 async def starkgang(event): 
@@ -34,7 +40,15 @@ async def starkgang(event):
     downloader = SmartDL( f"{STARK_SOCKS4}" , downloaded_file_name, progress_bar=False)
     downloader.start(blocking=False) 
     await borg.send_message(event.chat_id , SOCKS4_TXT)
-    await event.client.send_file(event.chat_id , downloaded_file_name) 
+    await event.client.send_file(
+        event.chat_id , 
+        downloaded_file_name,
+        thumb=sedpng,
+        caption=SOCKS4_TXT,
+        allow_cache=False,
+        force_document=True,
+        reply_to=message_id
+    )
  
 @borg.on(admin_cmd(pattern="proxysocks5")) 
 async def friday(event): 
@@ -44,5 +58,12 @@ async def friday(event):
     downloader = SmartDL( f"{STARK_SOCKS5}" , downloaded_file_name, progress_bar=False)
     downloader.start(blocking=False) 
     await borg.send_message(event.chat_id , SOCKS5_TXT)
-    await event.client.send_file(event.chat_id , downloaded_file_name) 
-    
+    await event.client.send_file(
+        event.chat_id , 
+        downloaded_file_name,
+        thumb=sedpng,
+        caption=SOCKS5_TXT,
+        allow_cache=False,
+        force_document=True,
+        reply_to=message_id
+    )
