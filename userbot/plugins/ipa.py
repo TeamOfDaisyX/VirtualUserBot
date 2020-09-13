c4db78090cdab796cf88cf8cc29421fffbc64e73
StarkGang <StarkGangZ@gmail.com>
2020-09-13 22:43:54 +0530

    Update ipa.py

--- userbot/plugins/ipa.py
+++ userbot/plugins/ipa.py
@@ -21,6 +21,7 @@ async def install(event):
     if event.fwd_from:
         return
     chat = event.pattern_match.group(1)
+    await event.edit(f"Starting To Install Plugins From {chat} ! Check PRIVATE GROUP for More Info !")
     documentss = await borg.get_messages(chat, None , filter=InputMessagesFilterDocument)
     total = int(documentss.total)
     total_doxx = range(0, total)
@@ -31,7 +32,6 @@ async def install(event):
             path1 = Path(downloaded_file_name)
             shortname = path1.stem
             load_module(shortname.replace(".py", ""))
-            await event.edit(f"Starting To Install Plugins From {chat} ! Check PRIVATE GROUP for More Info !")
             sed = f"Installing Plugins From {chat}"
             logger.info(sed)
             await borg.send_message(
