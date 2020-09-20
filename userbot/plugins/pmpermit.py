3b3f8941d616fdbff512be10f33474e7489f9ef8
StarkGang <StarkGangZ@gmail.com>
2020-09-20 20:13:39 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -10,7 +10,7 @@ from userbot.utils import admin_cmd
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-  WARN_PIC = "https://telegra.ph/file/82c595986872349e5ba1a.jpg"
+  WARN_PIC = "https://telegra.ph/file/a3c7924645009fa161f46.jpg"
 else:
   WARN_PIC = PMPERMIT_PIC
 
