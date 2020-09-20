174fa00fde74c79cf936189beceb1428adf5c09f
StarkGang <StarkGangZ@gmail.com>
2020-09-20 20:03:51 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -21,10 +21,10 @@ PREV_REPLY_MESSAGE = {}
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 CUSTOM_MIDDLE_PMP = str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "Protection By Friday 🇮🇳"
 USER_BOT_WARN_ZERO = "You Have Attempted To Spam Masters Inbox So Inorder To Avoid Over Spam , You Have Been Blocked By Userbot"
-USER_BOT_NO_WARN = ("Hello My Friend, This A Automated Message From Friday Security Service \n"
-                    f"User {DEFAULTUSER} Is Currently Offline !\n"
-                    "You Can Kindly Wait Till He Approves You \n"
-                    "And Don't Attempt To Spam His Inbox ! You May Get Blocked And Reported \n"
+USER_BOT_NO_WARN = ("**Hello My Friend, This A Automated Message From Friday Security Service** \n\n"
+                    f"__User {DEFAULTUSER} Is Currently Offline !__ \n\n"
+                    "`You Can Kindly Wait Till He Approves You` \n\n"
+                    "__And Don't Attempt To Spam His Inbox ! You May Get Blocked And Reported__ \n\n"
                     f"{CUSTOM_MIDDLE_PMP}")
 
 
