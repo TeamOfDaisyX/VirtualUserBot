d143104e8332b94d8c7cc8e689b8501fed89100c
StarkGang <StarkGangZ@gmail.com>
2020-09-21 13:08:18 +0530

    Fix

--- userbot/plugins/news.py
+++ userbot/plugins/news.py
@@ -19,16 +19,16 @@ async def _(event):
         return
     infintyvar = event.pattern_match.group(1)
     main_url = f"https://inshortsapi.vercel.app/news?category={infintyvar}"
-    await edit_or_reply(event, f"Ok ! Fectching {infintyvar} From inshortsapi Server And Sending To News Channel")
+    stuber = await edit_or_reply(event, f"Ok ! Fectching {infintyvar} From inshortsapi Server And Sending To News Channel")
+    await stuber.edit("All News Has Been Sucessfully Send To News Channel")
     starknews = requests.get(main_url).json()
     for item in starknews["data"]:
-                sedlyf = item["content"]
-                img = item["imageUrl"]
-                writter = item["author"]
-                dateis = item["date"]
-                readthis = item["readMoreUrl"]
-                titles = item["title"]
-                sed1 = img
-                sedm = (f"**Title : {titles}** \n{sedlyf} \nDate : {dateis} \nAuthor : {writter} \nReadMore : {readthis}")
-                await borg.send_file(newslog, sed1, caption=sedm)
-                await edit_or_reply(event, "All News Has Been Sucessfully Send To News Channel")
+        sedlyf = item["content"]
+        img = item["imageUrl"]
+        writter = item["author"]
+        dateis = item["date"]
+        readthis = item["readMoreUrl"]
+        titles = item["title"]
+        sed1 = img
+        sedm = (f"**Title : {titles}** \n{sedlyf} \nDate : {dateis} \nAuthor : {writter} \nReadMore : {readthis}")
+        await borg.send_file(newslog, sed1, caption=sedm)
