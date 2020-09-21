52d87495958ca96acac837fa2da613651fa10103
StarkGang <StarkGangZ@gmail.com>
2020-09-21 22:01:03 +0530

    Update spamwatch.py

--- userbot/plugins/spamwatch.py
+++ userbot/plugins/spamwatch.py
@@ -4,35 +4,35 @@ from telethon import events
 import spamwatch
 from var import Var
 
-spamwatchapi = Var.SPAMWATCH_API 
+spamwatchapi = Var.SPAMWATCH_API
 SPAM_PROTECT = "ENABLE"
 W_CHAT = Var.WHITE_CHAT
 
-
 if SPAM_PROTECT == "ENABLE":
- @bot.on(events.ChatAction)
- async def spam(event): 
-   if event.user_joined or event.user_added and not event.chat_id in W_CHAT and SPAM_PROTECT and spamwatchapi and not event.is_private:
-       chat = await event.get_chat()
-       admin = chat.admin_rights
-       creator = chat.creator   
-       if admin or creator:
-           return
-       sw = spamwatch.Client(spamwatchapi)
-       whoistheguy = await event.get_user()      
-       try:
-           sswatch = sw.get_ban(whoistheguy.id) 
-       except: 
-           return                   
-       if sswatch:                                                                                                
-                 try:
-                    await borg.edit_permissions(event.chat_id, whoistheguy.id, view_messages=False)    
-                    action = "`Banned 😢`" ; return await event.reply(f"**🛡️ SpamWatch Banned User Detected 🛡️** \n"                      
-            f"**Banned For :**  : `{sswatch.reason}`\n"
-            f"**User ID :** [{guser.id}](tg://user?id={guser.id})\n"                   
-            f"**Action Taken :**  : {action}"
-            "Kindly Appeal Here => @SpamWatch")                            
-                 except:                          
+    @bot.on(events.ChatAction)
+    async def spam(event):
+        if event.user_joined or event.user_added and not event.chat_id in W_CHAT and SPAM_PROTECT and spamwatchapi and not event.is_private:
+            chat = await event.get_chat()
+            admin = chat.admin_rights
+            creator = chat.creator
+            if admin or creator:
+                return
+            sw = spamwatch.Client(spamwatchapi)
+            whoistheguy = await event.get_user()
+            try:
+                sswatch = sw.get_ban(whoistheguy.id)
+            except:
+                return
+            if sswatch:
+                try:
+                    await borg.edit_permissions(event.chat_id, whoistheguy.id, view_messages=False)
+                    action = "`Banned ??`"
+                    return await event.reply(f"**??? SpamWatch Banned User Detected ???** \n"
+                                             f"**Banned For :**  : `{sswatch.reason}`\n"
+                                             f"**User ID :** [{guser.id}](tg://user?id={guser.id})\n"
+                                             f"**Action Taken :**  : {action}"
+                                             "Kindly Appeal Here => @SpamWatch")
+                except:
                     return 
-  
-    	
+
+
