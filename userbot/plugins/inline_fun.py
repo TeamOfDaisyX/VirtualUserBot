9a7782b8e587cdbaaf80d792cb78a78084bf61ad
StarkGang <StarkGangZ@gmail.com>
2020-09-28 22:03:31 +0530

    Hmm

--- userbot/plugins/inline_fun.py
+++ userbot/plugins/inline_fun.py
@@ -19,7 +19,7 @@ async def stats(event):
 
 
 @borg.on(admin_cmd(pattern="xogame$"))
-async def stats(event):
+async def gamez(event):
     if event.fwd_from:
         return
     botusername = "@xobot"
@@ -27,5 +27,17 @@ async def stats(event):
     if event.reply_to_msg_id:
         reply_to_id = await event.get_reply_message()
     tap = await bot.inline_query(botusername, noob) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+@borg.on(admin_cmd(pattern="wspr ?(.*)"))
+async def wspr(event):
+    if event.fwd_from:
+        return
+    wwwspr = event.pattern_match.group(1)
+    botusername = "@whisperBot"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, wwwspr) 
     await tap[0].click(event.chat_id)
     await event.delete()
