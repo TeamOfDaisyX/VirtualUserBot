c51605245692efa6e30bcf30a9d267378251e88e
StarkGang <StarkGangZ@gmail.com>
2020-09-28 20:51:28 +0530

    Update and rename inline.py to inline_fun.py

--- userbot/plugins/inline_fun.py
+++ userbot/plugins/inline_fun.py
@@ -5,12 +5,12 @@ from var import Var
 
 
 
-@borg.on(admin_cmd(pattern="showstats$"))
+@borg.on(admin_cmd(pattern="stat$"))
 async def stats(event):
     if event.fwd_from:
         return
     botusername = Var.TG_BOT_USER_NAME_BF_HER
-    noob = "noob"
+    noob = "stats"
     if event.reply_to_msg_id:
         reply_to_id = await event.get_reply_message()
     tap = await bot.inline_query(botusername, noob) 
