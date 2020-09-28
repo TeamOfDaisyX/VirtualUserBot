893d3063cc1948551ddf0dfee1c223c330e00acd
StarkGang <StarkGangZ@gmail.com>
2020-09-28 20:48:56 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -1,13 +1,11 @@
-"""Copy Right By Xtra-Telegram 2020 
-Fixed And Customized By @StarkXD 
-Don't Edit Before This Line"""
 import re
 from math import ceil
-
-from telethon import custom, events
-
+from userbot.plugins import inlinestats
+from telethon import custom, events, Button
+from userbot import ALIVE_NAME
 from userbot import CMD_LIST
 
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Friday"
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
     @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
@@ -24,8 +22,17 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 buttons=buttons,
                 link_preview=False,
             )
+        if query == "stats":
+           result = builder.article(
+           title="Stats",
+           text=f"**Showing Stats For {DEFAULTUSER}'s Friday** \n Note:Only Owner Can Check This \n (C) @FridayOT",
+           buttons = [
+                   [custom.Button.inline("Show Stats", data="terminator")],
+                   [Button.url("Repo", "https://github.com/StarkGang/FridayUserbot")],
+                   [Button.url("Join Channel", "t.me/Fridayot")],
+             ]
+         )
         await event.answer([result] if result else None)
-
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
             data=re.compile(b"helpme_next\((.+?)\)")
@@ -89,7 +96,15 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             reply_pop_up_alert = "Please get your own Userbot, and don't use mine!"
 
-
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"terminator")))
+    async def rip(event):
+            if event.query.user_id == bot.uid:
+                text = inlinestats
+                await event.answer(text, alert=True)
+            else:
+                txt = "You Can't View My Masters Stats"
+                await event.answer(txt, alert=True)
+                
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = 8
     number_of_cols = 2
