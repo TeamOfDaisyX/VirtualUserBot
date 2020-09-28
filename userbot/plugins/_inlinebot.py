2fa7314075ebf7557d32d51de6814c86125e1243
StarkGang <StarkGangZ@gmail.com>
2020-09-28 21:02:37 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -27,9 +27,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
            title="Stats",
            text=f"**Showing Stats For {DEFAULTUSER}'s Friday** \n Note:Only Owner Can Check This \n (C) @FridayOT",
            buttons = [
-                   [custom.Button.inline("Show Stats", data="terminator")],
-                   [Button.url("Repo", "https://github.com/StarkGang/FridayUserbot")],
-                   [Button.url("Join Channel", "t.me/Fridayot")],
+                   [custom.Button.inline("Show Stats 🚶", data="terminator")],
+                   [Button.url("Repo 🛡️", "https://github.com/StarkGang/FridayUserbot")],
+                   [Button.url("Join Channel 📃", "t.me/Fridayot")],
              ]
          )
         await event.answer([result] if result else None)
