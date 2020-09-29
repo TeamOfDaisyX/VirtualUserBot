6a683a1a0ad63e483da5671a2f3380117f6f0fc0
StarkGang <StarkGangZ@gmail.com>
2020-09-29 10:50:50 +0530

    Update send.py

--- userbot/plugins/send.py
+++ userbot/plugins/send.py
@@ -12,17 +12,20 @@ async def send(event):
         return
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
+    end = datetime.now()
+    time_taken_in_ms = (end - start).seconds
+    men = (f"Plugin Send Sucessfully. \nPlugin Name -> {input_str}.py \nTime Taken -> {time_taken_in_ms} ")
     the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
     start = datetime.now()
     await event.client.send_file(  # pylint:disable=E0602
         event.chat_id,
         the_plugin_file,
+        thumb=fridaythumb,
+        caption=men,
         force_document=True,
         allow_cache=False,
         reply_to=message_id,
     )
-    end = datetime.now()
-    time_taken_in_ms = (end - start).seconds
     await event.edit("Uploaded {} in {} seconds".format(input_str, time_taken_in_ms))
     await asyncio.sleep(5)
     await event.delete()
