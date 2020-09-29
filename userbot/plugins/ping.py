524e68d6ac123eb48f8764b044180020d1e1fd29
StarkGang <StarkGangZ@gmail.com>
2020-09-29 10:09:38 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,7 +1,32 @@
 from datetime import datetime
-
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
 
 @borg.on(admin_cmd(pattern="ping$"))
 @borg.on(sudo_cmd(pattern="ping$", allow_sudo=True))
@@ -12,4 +37,5 @@ async def _(event):
     start = datetime.now()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await starkislub.edit("**Pong!**\n`{}`\n For Assistance Join @FridayOT".format(ms))
+    uptime = get_readable_time((time.time() - Lastupdate))
+    await starkislub.edit(f"**Pong!**\n`{}` \nUptime -> {uptime}".format(ms))
