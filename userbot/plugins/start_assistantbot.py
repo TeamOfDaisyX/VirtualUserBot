16894b8fff8f1650f0838a929428932584c3398f
StarkGang <StarkGangZ@gmail.com>
2020-09-30 13:16:09 +0530

    Update start_assistantbot.py

--- userbot/plugins/start_assistantbot.py
+++ userbot/plugins/start_assistantbot.py
@@ -134,7 +134,7 @@ async def _(event):
          {}""".format(
             translated.src, lan, after_tr_text
         )
-    if event.from_id = bot.uid:
+    if event.from_id == bot.uid:
         await tgbot.send_message(event.chat_id, output_str)
     else:
         await tgbot.send_message(event.chat_id, "You Can't Access Me")
