44b354b4b4362fba97cf074e85abfb7d0cbfa90f
StarkGang <StarkGangZ@gmail.com>
2020-09-30 18:05:14 +0530

    Translater Support.

--- userbot/plugins/_assistant.py
+++ userbot/plugins/_assistant.py
@@ -2,7 +2,7 @@
 # 
 # Please Don't Kang Without Credits
 # A Plugin For Assistant Bot 
-
+# x0x
 
 from telethon import events, custom, Button
 from telethon.tl.types import (
@@ -10,6 +10,10 @@ from telethon.tl.types import (
     Chat,
     User
 )
+
+import emoji
+from googletrans import Translator
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from telethon.utils import get_display_name
 from userbot.utils import admin_cmd, sudo_cmd
 from userbot.uniborgConfig import Config
@@ -96,3 +100,30 @@ async def _(event):
            [Button.url("Join Channel 📃", "t.me/Fridayot")]
       ]
      )
+
+
+@tgbot.on(events.NewMessage(pattern="^/tr ?(.*)"))
+async def _(event):
+    input_str = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str or "gu"
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
+    else:
+        await tgbot.send_message(event.chat_id, "`.tr LanguageCode` as reply to a message")
+        return
+    text = emoji.demojize(text.strip())
+    lan = lan.strip()
+    translator = Translator()
+    translated = translator.translate(text, dest=lan)
+    after_tr_text = translated.text
+    output_str = (f"**Translated By Friday Assistant Bot** \n"
+                  f"Source {translated.src} \nTranslation {lan} \nWhat I Can Translate From This {after_tr_text}")
+    if event.from_id == bot.uid:
+        await tgbot.send_message(event.chat_id, output_str)
+    elif event.from_id is not bot.uid:
+        await tgbot.send_message(event.chat_id, "You Can't Access Me")
+    else:
+        await tgbot.send_message(event.chat_id, "Something Went Wrong 🤔")
