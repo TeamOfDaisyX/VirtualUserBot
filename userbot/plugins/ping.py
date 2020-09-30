58975846812721335452958fb85d479fecb5728b
StarkGang <StarkGangZ@gmail.com>
2020-09-30 09:12:28 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -40,4 +40,4 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - Lastupdate))
-    await starkislub.edit(f"**Pong!**\n`{ms}` \nUptime -> {uptime}")
+    await starkislub.edit(f"**█▀█ █▀█ █▄░█ █▀▀ █ \n█▀▀ █▄█ █░▀█ █▄█ ▄**\n ➲ `{ms}` \n ➲ `{uptime}`")
