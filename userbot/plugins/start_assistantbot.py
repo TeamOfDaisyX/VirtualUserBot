b6e6c187f65f124b9e7bb653b2e33a44b498525e
StarkGang <StarkGangZ@gmail.com>
2020-09-30 17:27:12 +0530

    Update start_assistantbot.py

--- userbot/plugins/start_assistantbot.py
+++ userbot/plugins/start_assistantbot.py
@@ -120,7 +120,7 @@ async def _(event):
     translated = translator.translate(text, dest=lan)
     after_tr_text = translated.text
     output_str = (f"**Translated By Friday Assistant Bot** \n"
-                  "Source {translated.src} \nTranslation {lan} & {after_tr_text}")
+                  f"Source {translated.src} \nTranslation {lan} & {after_tr_text}")
     if event.from_id == bot.uid:
         await tgbot.send_message(event.chat_id, output_str)
     elif event.from_id is not bot.uid:
