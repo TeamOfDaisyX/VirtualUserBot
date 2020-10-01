f5135698528871f7cc58ce9f322a3dfb41670d75
StarkGang <StarkGangZ@gmail.com>
2020-10-01 12:55:40 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,7 +5,7 @@ import telethon.utils
 from telethon import TelegramClient
 
 from userbot import bot
-from userbot.utils import load_module
+from userbot.utils import load_module, start_assistant
 from var import Var
 
 
