95e4bb1b30cf3ff3f406b2c46cf79d6d6f8ccff9
StarkGang <StarkGangZ@gmail.com>
2020-10-03 17:05:42 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -157,14 +157,11 @@ if Var.PRIVATE_GROUP_ID is not None:
             # don't log verified accounts
 
             return
-
-        if any([x in event.raw_text for x in ("/start", "1", "2", "3", "4", "5")]):
-            return
-
+        
         if not pmpermit_sql.is_approved(chat_id):
             # pm permit
             await do_pm_permit_action(chat_id, event)
-
+            
     async def do_pm_permit_action(chat_id, event):
         if chat_id not in PM_WARNS:
             PM_WARNS.update({chat_id: 0})
