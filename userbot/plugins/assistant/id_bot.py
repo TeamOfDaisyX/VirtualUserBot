3f8736b4965b95e6a2b03ed464c3e62defeda492
StarkGang <StarkGangZ@gmail.com>
2020-10-10 14:23:32 +0530

    Update id_bot.py

--- userbot/plugins/assistant/id_bot.py
+++ userbot/plugins/assistant/id_bot.py
@@ -31,7 +31,7 @@ from userbot import Lastupdate, bot
 from userbot.plugins.sql_helper.botusers_sql import add_me_in_db, his_userid
 from userbot.plugins.sql_helper.idadder_sql import add_usersid_in_db, get_all_users
 
-@tgbot.on(events.NewMessage(pattern="^/id ?(.*)", func=lambda e: e.sender_id == bot.uid))
+@tgbot.on(events.NewMessage(pattern="^/id", func=lambda e: e.sender_id == bot.uid))
 async def _(event):
     if event.reply_to_msg_id:
         await event.get_input_chat()
@@ -39,17 +39,17 @@ async def _(event):
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
             await tgbot.send_message(
-                bot.uid,
+                event.chat_id,
                 "Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(
                     str(event.chat_id), str(r_msg.from_id), bot_api_file_id
                 )
             )
         else:
             await tgbot.send_message(
-                bot.uid,
+                event.chat_id,
                 "Current Chat ID: `{}`\nFrom User ID: `{}`".format(
                     str(event.chat_id), str(r_msg.from_id)
                 )
             )
     else:
-        await tgbot.send_message(bot.uid, "Current Chat ID: `{}`".format(str(event.chat_id)))
+        await tgbot.send_message(event.chat_id, "Current Chat ID: `{}`".format(str(event.chat_id)))
