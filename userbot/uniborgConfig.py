d37a6f48f037f8240fe56824e90d9f2539d89ea6
StarkGang <StarkGangZ@gmail.com>
2020-10-10 11:40:18 +0530

    Add files via upload

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -137,6 +137,7 @@ if ENV:
         PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", False))
         NEWS_CHANNEL_ID = int(os.environ.get("NEWS_CHANNEL_ID", False))
         PM_DATA = os.environ.get("PM_DATA", "ENABLE")
+        TAG_FEATURE = os.environ.get("TAG_FEATURE", "ENABLE")
 
 else:
 
