c246fb73d45bccc2fd1bbf8c7709a4e5b66e9df7
StarkGang <StarkGangZ@gmail.com>
2020-10-10 12:55:20 +0530

    Update botusers_sql.py

--- userbot/plugins/sql_helper/botusers_sql.py
+++ userbot/plugins/sql_helper/botusers_sql.py
@@ -31,24 +31,24 @@ class Users(BASE):
     """ Table to store the received messages """
     __tablename__ = "users"
     message_id = Column(Integer, primary_key=True)
-    chat_id = Column(String(14))
+    users_ids = Column(String(14))
     um_id = Column(Integer)
 
-    def __init__(self, message_id, chat_id, um_id):
+    def __init__(self, message_id, users_ids, um_id):
         self.message_id = message_id
-        self.chat_id = str(chat_id)  # ensure string
+        self.users_ids = str(users_ids)  # ensure string
         self.um_id = um_id
 
     def __repr__(self):
-        return "<User %s>" % self.chat_id
+        return "<User %s>" % self.users_ids
 
 
 Users.__table__.create(checkfirst=True)
 
 
-def add_me_in_db(message_id: int, chat_id: int, um_id: int):
+def add_me_in_db(message_id: int, users_ids: int, um_id: int):
     """ add the message to the table """
-    __user = Users(message_id, str(chat_id), um_id)
+    __user = Users(message_id, str(users_ids), um_id)
     SESSION.add(__user)
     SESSION.commit()
 
@@ -56,6 +56,6 @@ def his_userid(message_id: int):
     """ get the user_id from the message_id """
     try:
         s__ = SESSION.query(Users).get(str(message_id))
-        return int(s__.chat_id), s__.um_id
+        return int(s__.users_ids), s__.um_id
     finally:
         SESSION.close()
