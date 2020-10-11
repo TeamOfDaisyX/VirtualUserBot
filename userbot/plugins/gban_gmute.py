3bb0b05d1e705151263d0ace3fa0110c96c6a82d
StarkGang <StarkGangZ@gmail.com>
2020-10-11 20:24:08 +0530

    Fix

--- userbot/plugins/gban_gmute.py
+++ userbot/plugins/gban_gmute.py
@@ -2,11 +2,10 @@
 # PORTED BY @STARKXD
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
 
-from userbot import CMD_HELP
+from userbot import CMD_HELP, admin_cmd
 
 
-# @javes.on(rekcah05(pattern=f"gban(?: |$)(.*)", allow_sudo=True))
-@command(outgoing=True, pattern="^.gban(?: |$)(.*)")
+@borg.on(admin_cmd(pattern="gban ?(.*)"))
 async def gspider(userbot):
     lol = userbot
     sender = await lol.get_sender()
@@ -27,7 +26,7 @@ async def gspider(userbot):
     else:
         userbot.chat.title
     try:
-        user, reason = await get_user_from_event(userbot)
+        user, reason = await get_full_user(userbot)
     except:
         pass
     try:
@@ -72,8 +71,7 @@ async def gspider(userbot):
     )
 
 
-# @javes.on(rekcah05(pattern=f"ungban(?: |$)(.*)", allow_sudo=True))
-@command(outgoing=True, pattern="^.ungban(?: |$)(.*)")
+@borg.on(admin_cmd(pattern="ungban ?(.*)"))
 async def gspider(userbot):
     lol = userbot
     sender = await lol.get_sender()
@@ -94,14 +92,14 @@ async def gspider(userbot):
     else:
         userbot.chat.title
     try:
-        user, reason = await get_user_from_event(userbot)
+        user, reason = await get_full_user(userbot)
     except:
         pass
     try:
         if not reason:
             reason = "Private"
     except:
-        return await friday.edit("Use In Public Chats , Or In PM")
+        return await friday.edit("Someting Went Wrong 🤔")
     if user:
         if user.id == 1263617196:
             return await friday.edit("**You Cant Ungban A Dev !**")
@@ -136,6 +134,68 @@ async def gspider(userbot):
         f"**UNGBANNED // USER - [{user.first_name}](tg://user?id={user.id}) CHATS : {a} **"
     )
 
+
+import html
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+
+
+
+async def get_full_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(
+                    previous_message.forward.from_id
+                    or previous_message.forward.channel_id
+                )
+            )
+            return replied_user, None
+        else:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.from_id)
+            )
+            return replied_user, None
+    else:
+        input_str = None
+        try:
+            input_str = event.pattern_match.group(1)
+        except IndexError as e:
+            return None, e
+        if event.message.entities is not None:
+            mention_entity = event.message.entities
+            probable_user_mention_entity = mention_entity[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            else:
+                try:
+                    user_object = await event.client.get_entity(input_str)
+                    user_id = user_object.id
+                    replied_user = await event.client(GetFullUserRequest(user_id))
+                    return replied_user, None
+                except Exception as e:
+                    return None, e
+        elif event.is_private:
+            try:
+                user_id = event.chat_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
+        else:
+            try:
+                user_object = await event.client.get_entity(int(input_str))
+                user_id = user_object.id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
 
 CMD_HELP.update(
     {
