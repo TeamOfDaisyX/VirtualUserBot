0f97c4dc30ba48b34a9a22f553d2c92ba490ca12
StarkGang <StarkGangZ@gmail.com>
2020-10-11 20:05:44 +0530

    Update getuser.py

--- userbot/function/getuser.py
+++ userbot/function/getuser.py
@@ -1,3 +1,14 @@
+import html
+
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+
+
+
 async def get_full_user(event):
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
