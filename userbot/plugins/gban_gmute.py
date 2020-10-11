3bc4bc77ed38bf5c2b8edbf4deff185b70ad5ba1
StarkGang <StarkGangZ@gmail.com>
2020-10-11 20:30:29 +0530

    Update gban_gmute.py

--- userbot/plugins/gban_gmute.py
+++ userbot/plugins/gban_gmute.py
@@ -140,7 +140,6 @@ from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 
