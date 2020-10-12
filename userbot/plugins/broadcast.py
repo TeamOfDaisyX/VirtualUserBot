65d56a23eed578030cb1f1cf8be5794576221a53
StarkGang <StarkGangZ@gmail.com>
2020-10-12 10:26:47 +0530

    👌

--- userbot/plugins/broadcast.py
+++ userbot/plugins/broadcast.py
@@ -1,8 +1,20 @@
-# Fully Written by @HeisenbergTheDanger (Keep credits else gay)
-# Permission Seeked By @StarkXD - Approved
+#    Copyright (C) By @HeisenbergTheDanger
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 import asyncio
-
 from telethon.tl.types import InputMediaUploadedPhoto
 from uniborg.util import admin_cmd, edit_or_reply, sudo_cmd
 
