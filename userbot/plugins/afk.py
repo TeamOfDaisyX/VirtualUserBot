e7c989a1caa5db39085728bd828c7513e4392cb9
StarkGang <StarkGangZ@gmail.com>
2020-10-16 11:15:07 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -18,7 +18,7 @@ last_afk_message = {}
 afk_start = {}
 
 
-@friday.on(admin_cmd(pattern="afk ?(.*)"))  # pylint:disable=E0602
+@friday.on(friday_on_cmd(pattern="afk ?(.*)"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
