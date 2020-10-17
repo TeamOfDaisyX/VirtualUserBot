bb3a5d25e4e6479dfb630d2db471c101273ba8e1
StarkGang <StarkGangZ@gmail.com>
2020-10-17 10:20:22 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -155,7 +155,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     async def rip(event):
         chat_m = await event.get_chat()
         him_id = event.query.user_id
-        name_is = event.query.user_id.first_name
+        name_is = event.query.first_name
         await event.edit("Choice Accepted ✔️")
         text2 = (
             "Ok. Please Wait Until My Master Approves. Don't Spam Or Try Anything Stupid. \nThank You For Contacting Me."
