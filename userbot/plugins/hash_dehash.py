366c7b43cf75342dbc3125d27be642a74a01b8a8
StarkGang <StarkGangZ@gmail.com>
2020-10-20 21:17:35 +0530

    Update hash_dehash.py

--- userbot/plugins/hash_dehash.py
+++ userbot/plugins/hash_dehash.py
@@ -1,10 +1,8 @@
-# Idea From Userge
-
 import base64
 from userbot.utils import friday_on_cmd, sudo_cmd, edit_or_reply
 
 
-@friday.on(friday_on_cmd(pattern="hash ?(.*)"))
+@friday.on(friday_on_cmd(pattern="bencode ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -17,20 +15,16 @@ async def _(event):
     except:
         borg.send_message("SOM3THING W3NT WRONG !")
 
-@friday.on(friday_on_cmd(pattern="dehash ?(.*)"))
+@friday.on(friday_on_cmd(pattern="bdecode ?(.*)"))
 async def _(event):
     if event.fwd_from:
             return
     starky = event.pattern_match.group(1)
     try:
-       base64_bytes = starky.encode("ascii")
-       sample_string_bytes = base64.b64decode(base64_bytes)
-       sample_string = sample_string_bytes.decode("ascii")
-       event.edit(f"**DECODED STRING** : `{sample_string}`")
+       base64_bytez = starky.encode("ascii")
+       sample_string_bytez = base64.b64decode(base64_bytez)
+       sample_strings = sample_string_bytez.decode("ascii")
+       event.edit(f"**DECODED STRING** : `{sample_strings}`")
     except:
         event.edit("Can't Decoded Probably Due To Invalid String")
-
-
-
-
 
