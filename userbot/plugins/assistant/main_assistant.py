947ac212187e5c111d69d458c3a04aa534226e5f
StarkGang <StarkGangZ@gmail.com>
2020-10-21 09:28:14 +0530

    Update main_assistant.py

--- userbot/plugins/assistant/main_assistant.py
+++ userbot/plugins/assistant/main_assistant.py
@@ -151,6 +151,8 @@ async def all_messages_catcher(event):
 @tgbot.on(events.NewMessage(func=lambda e: e.is_private))
 async def sed(event):
     msg = await event.get_reply_message()
+    if not msg.text:
+        return
     real_nigga = msg.id
     msg_s = event.raw_text
     user_id, reply_message_id = his_userid(
