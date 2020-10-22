f36ba440b97126892d0363fdd591f26011b402c3
StarkGang <StarkGangZ@gmail.com>
2020-10-22 18:25:22 +0530

    Update fridayfunction.py

--- userbot/function/fridayfunction.py
+++ userbot/function/fridayfunction.py
@@ -22,17 +22,3 @@ def stark_finder(s, first, last):
 def id_generator(size=64, chars=string.ascii_uppercase + string.digits):
     return "".join(random.choice(chars) for _ in range(size))
 
-# https://docs.python.org/3/library/asyncio-subprocess.html 
-# https://github.com/code-rgb/USERGE-X/blob/master/userge/utils/tools.py#L79
-async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
-    """ run command in terminal """
-    args = shlex.split(cmd)
-    process = await asyncio.create_subprocess_exec(*args,
-                                                   stdout=asyncio.subprocess.PIPE,
-                                                   stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await process.communicate()
-    return (stdout.decode('utf-8', 'replace').strip(),
-            stderr.decode('utf-8', 'replace').strip(),
-            process.returncode,
-            process.pid)
-
