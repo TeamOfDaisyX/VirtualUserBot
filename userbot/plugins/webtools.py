6f7dc97cbc503d813e8b639b205cb32c2a6a164c
StarkGang <StarkGangZ@gmail.com>
2020-10-22 10:09:06 +0530

    Update webtools.py

--- userbot/plugins/webtools.py
+++ userbot/plugins/webtools.py
@@ -8,11 +8,12 @@ from iplookup import iplookup
 
 
 @friday.on(friday_on_cmd(pattern="wshot ?(.*)"))
+@friday.on(sudo_cmd(pattern="wshot ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     urlissed = event.pattern_match.group(1)
-    sedlyfstarky = await event.edit("Capturing Webshot, Stay Tuned.")
+    sedlyfstarky = await edit_or_reply(event, "Capturing Webshot, Stay Tuned.")
     driver = webdriver.Chrome()
     driver.get(urlissed)
     stark_img = driver.get_screenshot_as_file('Webshot-@fridayot.png')
@@ -22,10 +23,12 @@ async def _(event):
     
     
 @friday.on(friday_on_cmd(pattern="lp ?(.*)"))
+@friday.on(sudo_cmd(pattern="lp ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
             return
     try:
+        tfbro = await edit_or_reply(event, "Wait Fetching Website Info")
         gone = event.pattern_match.group(1)
         url = f"https://api.ip2whois.com/v1?key=free&domain="+gone
         await event.edit("Fecthing Website Info ! Stay Tuned. This may take some time ;)")
@@ -38,16 +41,18 @@ async def _(event):
          f'\nLast updated => {sed["update_date"]} \nExpiry Date => {sed["expire_date"]} \nDomain Age => {sed["domain_age"]}'
          f'\nOwner => {km["name"]} \nCountry => {km["country"]} \nState => {km["region"]}'
          f'\nPhone Number => {km["phone"]} \nDomain Ip => {okthen}')
-        await event.edit(sedlyf)
+        await tfbro.edit(sedlyf)
     except Exception as e:
-        await event.edit(f'Something went wrong \nERROR => {e}')
+        await tfbro.edit(f'Something Went Wrong. MayBe Website Wrong.)
 
 
 @friday.on(friday_on_cmd(pattern="bin ?(.*)"))
+@friday.on(sudo_cmd(pattern="bin ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     try:
+        tfsir = await edit_or_reply(event, "Wait Fetching Bin Info")
         kek = event.pattern_match.group(1)
         url = f"https://lookup.binlist.net/{kek}"
         midhunkm = requests.get(url=url).json()
@@ -57,12 +62,13 @@ async def _(event):
            f"<b><u>Scheme</u></b> ➠ <code>{midhunkm['scheme']}</code> \n"
            f"<b><u>Brand</u></b> ➠ <code>{midhunkm['brand']}</code> \n"
            f"<b><u>Country</u></b> ➠ <code>{kekvro['name']} {kekvro['emoji']}</code> \n")
-        await event.edit(data_is, parse_mode="HTML")
+        await tfsir.edit(data_is, parse_mode="HTML")
     except:
-        await event.edit("Not a Valid Bin Or Don't Have Enough Info.")
+        await tfsir.edit("Not a Valid Bin Or Don't Have Enough Info.")
 
                    
 @friday.on(friday_on_cmd(pattern="iban ?(.*)"))
+@friday.on(sudo_cmd(pattern="iban ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -70,6 +76,7 @@ async def _(event):
     api = f"https://openiban.com/validate/{inputs}?getBIC=true&validateBankCode=true"
     lol = requests.get(url=api).json()
     try:
+        tfhm = await edit_or_reply(event, "Wait Fetching IBAN Info")
         banks = lol["bankData"]
         kek = (f"<b><u>VALID</u></b> ➠ <code>{lol['valid']}</code> \n"
            f"<b><u>IBAN</u></b> ➠ <code>{lol['iban']}</code> \n"
@@ -78,16 +85,18 @@ async def _(event):
            f"<b><u>ZIP</u></b> ➠ <code>{banks['zip']}</code> \n"
            f"<b><u>CITY</u></b> ➠ <code>{banks['city']}</code> \n"
            f"<b><u>BIC</u></b> ➠ <code>{banks['bic']}</code> \n")
-        await event.edit(kek, parse_mode="HTML")
+        await tfhm.edit(kek, parse_mode="HTML")
     except:
-        await event.edit(f"Invalid IBAN Or Doesn't Have Enough Info")
+        await tfhm.edit(f"Invalid IBAN Or Doesn't Have Enough Info")
 
                    
 @friday.on(friday_on_cmd(pattern="gitdl ?(.*)"))
+@friday.on(sudo_cmd(pattern="gitdl ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     try:
+        kekman = await edit_or_reply(event, "Fetching Repo")
         inputs = event.pattern_match.group(1)
         sed = event.pattern_match.group(1)
         if sed:
@@ -97,8 +106,8 @@ async def _(event):
         gitrepo = stark[1]
         gitbranch = stark[2]
         link = f"https://github.com/{gitusername}/{gitrepo}/archive/{gitbranch}.zip"
-        await event.edit("Uploading... Stark Tuned.")
-        await event.delete()
+        await kekman.edit("Uploading... Stark Tuned.")
+        await kemman.delete()
         await borg.send_file(event.chat_id, file=link, caption="You Repo Achieve File.")
     except:
         await borg.send_message(event.chat_id, "**Usage** : `.gitdl <gitusername> <gitrepo> <gitbranch>`")
