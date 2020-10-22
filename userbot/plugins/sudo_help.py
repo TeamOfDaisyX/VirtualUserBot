812dbc4e121c961275f4bf148a50701ab8ce628b
StarkGang <StarkGangZ@gmail.com>
2020-10-22 10:36:59 +0530

    Update sudo_help.py

--- userbot/plugins/sudo_help.py
+++ userbot/plugins/sudo_help.py
@@ -6,6 +6,7 @@ async def _(event):
     if event.fwd_from:
         return
     plugin_name = event.pattern_match.group(1)
+    help_string = ""
     for i in CMD_LIST[plugin_name]:
         help_string += i
         help_string += "\n"
