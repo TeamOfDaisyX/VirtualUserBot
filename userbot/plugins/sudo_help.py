1db2ca61bf552b84c72df36f442efade0eaa47f5
StarkGang <StarkGangZ@gmail.com>
2020-10-22 10:30:28 +0530

    Create sudo_help.py

---
+++ userbot/plugins/sudo_help.py
@@ -0,0 +1,12 @@
+from userbot import CMD_LIST
+from userbot.utils import sudo_cmd, edit_or_reply
+
+@friday.on(sudo_cmd(pattern="help ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    plugin_name = event.pattern_match.group(1)
+    for i in CMD_LIST[plugin_name]:
+        help_string += i
+        help_string += "\n"
+    await event.reply(help_string)
