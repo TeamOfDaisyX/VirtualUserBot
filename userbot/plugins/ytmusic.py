9fac11611252a34c672a18477224c64c727a185a
StarkGang <StarkGangZ@gmail.com>
2020-10-23 13:04:55 +0530

    Update ytmusic.py

--- userbot/plugins/ytmusic.py
+++ userbot/plugins/ytmusic.py
@@ -8,24 +8,21 @@ from userbot.utils import sudo_cmd, friday_on_cmd
 async def _(event):
     if event.fwd_from:
         return
-    try:
-        urlissed = event.pattern_match.group(1)
-        await event.edit("Fecthing Song....")
-        search = SearchVideos(f"{urlissed}", offset = 1, mode = "dict", max_results = 1)
-        mi = search.result()
-        mio = mi['search_result']
-        mo = mio[0]['link']
-        thum = mio[0]['title']
-        thumb_nail = mio[0]['thumbnails']
-        kek = thumb_nail[0]
-        do = await borg.download(kek, Config.TMP_DOWNLOAD_DIRECTORY)
-        youtube_video_url = f"{mo}"
-        yt_obj = YouTube(youtube_video_url)
-        if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-            os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-        path = Config.TMP_DOWNLOAD_DIRECTORY
-        keks = yt_obj.streams.get_audio_only().download(output_path=path, filename=f'{thum}')
-        await borg.send_file(file=keks, thumb=kek, caption=f"{thum}")
-    except:
-        await event.edit("Something Went Wrong")
+    urlissed = event.pattern_match.group(1)
+    await event.edit("Fecthing Song....")
+    search = SearchVideos(f"{urlissed}", offset = 1, mode = "dict", max_results = 1)
+    mi = search.result()
+    mio = mi['search_result']
+    mo = mio[0]['link']
+    thum = mio[0]['title']
+    thumb_nail = mio[0]['thumbnails']
+    kek = thumb_nail[0]
+    do = await borg.download(kek, Config.TMP_DOWNLOAD_DIRECTORY)
+    youtube_video_url = f"{mo}"
+    yt_obj = YouTube(youtube_video_url)
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    path = Config.TMP_DOWNLOAD_DIRECTORY
+    keks = yt_obj.streams.get_audio_only().download(output_path=path, filename=f'{thum}')
+    await borg.send_file(file=keks, thumb=kek, caption=f"{thum}")
     
