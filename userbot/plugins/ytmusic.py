5e2d2bc1d93600bbfd7b3fdd7e5b3996b149a602
StarkGang <StarkGangZ@gmail.com>
2020-10-23 13:30:01 +0530

    Update ytmusic.py

--- userbot/plugins/ytmusic.py
+++ userbot/plugins/ytmusic.py
@@ -24,7 +24,7 @@ async def _(event):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     path = Config.TMP_DOWNLOAD_DIRECTORY
     keks = yt_obj.streams.get_audio_only().download(output_path=path, filename=f'{thum}')
-    kek = await event.edit("Now Sending To Tg ?")
-    await borg.send_file(event.chat_id, file=keks, thumb=do, caption=f"{thum}")
+    kek = await event.edit("Song Found ! Uploading This Song..")
+    await borg.send_file(event.chat_id, file=keks, force_document=False, thumb=do, caption=f"{thum}, supports_streaming=True")
     await kek.edit("Done!")
     
