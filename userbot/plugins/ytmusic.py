870e3a58e221d6a86a8ac07c425a013fb065dcc2
StarkGang <StarkGangZ@gmail.com>
2020-10-24 21:44:00 +0530

    Small Hacks ;)

--- userbot/plugins/ytmusic.py
+++ userbot/plugins/ytmusic.py
@@ -4,22 +4,24 @@ import os
 import wget
 from userbot.uniborgConfig import Config
 import asyncio
-from userbot.utils import sudo_cmd, friday_on_cmd
+from userbot.utils import sudo_cmd, friday_on_cmd, edit_or_reply
+
+
 @friday.on(friday_on_cmd(pattern="ytmusic ?(.*)"))
 @friday.on(sudo_cmd(pattern="ytmusic ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     urlissed = event.pattern_match.group(1)
-    await event.edit(f"`Getting {urlissed} From Youtube Servers. Please Wait.`")
+    myself_stark = await edit_or_reply(event, f"`Getting {urlissed} From Youtube Servers. Please Wait.`")
     search = SearchVideos(f"{urlissed}", offset = 1, mode = "dict", max_results = 1)
     mi = search.result()
     mio = mi['search_result']
     mo = mio[0]['link']
     thum = mio[0]['title']
-    thumb_nail = mio[0]['thumbnails']
+    friday = mio[0]['id']
     thums = mio[0]['channel']
-    kek = thumb_nail[0]
+    kek = f"http://i3.ytimg.com/vi/{friday}/maxresdefault.jpg"
     youtube_video_url = f"{mo}"
     yt_obj = YouTube(youtube_video_url)
     await asyncio.sleep(0.6)
@@ -28,7 +30,7 @@ async def _(event):
     path = Config.TMP_DOWNLOAD_DIRECTORY
     sedlyf = wget.download(kek, out = path)
     keks = yt_obj.streams.get_audio_only().download(output_path=path, filename=f'{thum}')
-    kekm = await event.edit("`Song Downloaded Sucessfully. Let Me Upload it.`")
+    await myself_stark.edit("`Song Downloaded Sucessfully. Let Me Upload it Here.`")
     renamee = keks
     pre, ext = os.path.splitext(renamee)
     new_extension = ".mp3"
@@ -43,7 +45,7 @@ async def _(event):
                 performer=thums,
                 thumb = sedlyf,
                 supports_streaming=True) 
-    await kekm.edit("Song Uploaded. By (C) @FridayOT")
+    await myself_stark.edit("`Song Uploaded. By (C) @FridayOT`")
     for files in (sedlyf, km):
         if files and os.path.exists(files):
             os.remove(files)
