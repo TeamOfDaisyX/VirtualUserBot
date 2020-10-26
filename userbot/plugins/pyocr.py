f9d472abc27b9d8bb52dce02ffa17a7af6ef8f74
StarkGang <StarkGangZ@gmail.com>
2020-10-26 20:46:43 +0530

    Update pyocr.py

--- userbot/plugins/pyocr.py
+++ userbot/plugins/pyocr.py
@@ -6,20 +6,25 @@ import pytesseract
 import os
 from userbot.utils import friday_on_cmd, sudo_cmd
 from userbot.uniborgConfig import Config
-
-@friday.on(friday_on_cmd(pattern="readit$"))
+import html
+@friday.on(friday_on_cmd(pattern="ocr$"))
 async def _(event):
     global images
     if event.fwd_from:
         return
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    if event.reply_to_msg_id:
-        imagez = await borg.download_media(
-            await event.get_reply_message(), Config.TMP_DOWNLOAD_DIRECTORY
-        )
-    pytesseract.pytesseract.tesseract_cmd = "/app/.apt/usr/bin/tesseract"
-    results = (pytesseract.image_to_string(Image.open(imagez)))
-    await event.edit("READ :" + results)
-    if os.path.exists(results):
-        os.remove(results)
+    try:
+        await event.edit('`Reading..`')
+        if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+            os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+        if event.reply_to_msg_id:
+            imagez = await borg.download_media(
+                await event.get_reply_message(), Config.TMP_DOWNLOAD_DIRECTORY
+            )
+        pytesseract.pytesseract.tesseract_cmd = "/app/.apt/usr/bin/tesseract"
+        results = (pytesseract.image_to_string(Image.open(imagez)))
+        mk = f"<b><u> OCR </u></b> \n<b></u>Here is What I Can Read From This.</u></b> \n<code>{results}</code>"
+        await event.edit(mk, parse_mode="HTML")
+        if os.path.exists(results):
+            os.remove(results)
+    except:
+        event.edit("`Buy Me New Glass. I can't Read it...`")
