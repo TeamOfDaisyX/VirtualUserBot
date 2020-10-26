352d342d706fe90a353a6b12c5ba6f6142d111a3
StarkGang <StarkGangZ@gmail.com>
2020-10-26 13:39:11 +0530

    Update ytmusic.py

--- userbot/plugins/ytmusic.py
+++ userbot/plugins/ytmusic.py
@@ -5,7 +5,6 @@ import wget
 from userbot.uniborgConfig import Config
 import asyncio
 from userbot.utils import sudo_cmd, friday_on_cmd, edit_or_reply
-from downloader_cli.download import Download
 
 @friday.on(friday_on_cmd(pattern="ytmusic ?(.*)"))
 @friday.on(sudo_cmd(pattern="ytmusic ?(.*)", allow_sudo=True))
@@ -21,29 +20,28 @@ async def _(event):
     thum = mio[0]['title']
     fridayz = mio[0]['id']
     thums = mio[0]['channel']
-    kekme = f"http://img.youtube.com/vi/" + fridayz + "/maxresdefault.jpg"
-    youtube_video_url = f"{mo}"
-    yt_obj = YouTube(youtube_video_url)
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
     await asyncio.sleep(0.6)
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if not os.path.isdir('./music/'):
+        os.makedirs('./music/')
     path = Config.TMP_DOWNLOAD_DIRECTORY
-    sedlyf = Download(kekme).download()
-    keks = yt_obj.streams.get_audio_only().download(output_path=path, filename=f'{thum}')
-    await myself_stark.edit("`Song Downloaded Sucessfully. Let Me Upload it Here.`")
-    renamee = keks
-    pre, ext = os.path.splitext(renamee)
-    new_extension = ".mp3"
-    hmm = os.rename(renamee, pre + new_extension)
-    km = pre + new_extension
+    sedlyf = wget.download(kekme, out = path)
+    stark = (f'youtube-dl --force-ipv4 -q -o "./music/%(title)s.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 128k ' + mo)
+    os.system(stark)
+    await asyncio.sleep(4)
+    km = f"./music/{thum}.mp3"
+    if os.path.exists(km):
+        await myself_stark.edit("`Song Downloaded Sucessfully. Let Me Upload it Here.`")
+    else:
+        await myself_stark.edit("`SomeThing Went Wrong. Try Again After Sometime..`")
     capy = f"**Song Name ➠** `{thum}` \n**Requested For ➠** `{urlissed}` \n**Channel ➠** `{thums}` \n**Link ➠** `{mo}`" 
     await borg.send_file(event.chat_id,
                 km,
                 force_document=False,
                 allow_cache=False,
                 caption=capy,
+                thumb=sedlyf,
                 performer=thums,
-                thumb = sedlyf,
                 supports_streaming=True) 
     await myself_stark.edit("`Song Uploaded. By (C) @FridayOT`")
     for files in (sedlyf, km):
