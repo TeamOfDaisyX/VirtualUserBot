a2eabd8b2c89be9aa921fadbeb38a09fdc569794
StarkGang <StarkGangZ@gmail.com>
2020-10-27 10:09:21 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -6,7 +6,7 @@ Syntax: .update
 import asyncio
 import os
 import sys
-
+from userbot.uniborgConfig import Config
 import git
 
 from userbot.utils import friday_on_cmd
@@ -18,7 +18,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/Starkgang/FridayUserbot"
+OFFICIAL_UPSTREAM_REPO = Config.UPSTREAM_REPO
 BOT_IS_UP_TO_DATE = "`The userbot is up-to-date.\nThank you for Using this Service.`"
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n" "changelog: \n\n{changelog}\n" "updating ..."
