a266b57cd0bfbecc17ae16e1ec7955a622266095
StarkGang <StarkGangZ@gmail.com>
2020-11-05 13:19:26 +0530

    Update utils.py

--- fridaybot/utils.py
+++ fridaybot/utils.py
@@ -462,6 +462,19 @@ async def edit_or_reply(event, text):
 
 
 
+#    Copyright (C) Midhun KM 2020
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+# 
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 def assistant_cmd(add_cmd, is_args=False):
     def cmd(func):
