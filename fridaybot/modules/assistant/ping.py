bdb7a88fe4dbf3c99cd8ac2a1d374afcb8824248
StarkGang <StarkGangZ@gmail.com>
2020-11-05 13:18:11 +0530

    Update ping.py

--- fridaybot/modules/assistant/ping.py
+++ fridaybot/modules/assistant/ping.py
@@ -1,8 +1,16 @@
-# Copyright (C) Midhun KM 
+
+#    Copyright (C) Midhun KM 2020
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
 # 
-# Please Don't Kang Without Credits
-# A Plugin For Assistant Bot 
-# x0x
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from telethon import events, custom, Button
 from telethon.tl.types import (
