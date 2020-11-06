528168e50f99c4c2fc289ef0ac5f241c8f128bbb
StarkGang <StarkGangZ@gmail.com>
2020-11-06 18:36:41 +0530

    Update _inlinebot.py

--- fridaybot/modules/_inlinebot.py
+++ fridaybot/modules/_inlinebot.py
@@ -120,9 +120,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                   © Userbot".format(
             plugin_name
         )
-        try:
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-        except:
+        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        if len(reply_pop_up_alert) > 4095:
             crackexy = "Sir. The String Was Too Big So Me Sending Here As Paste.."
             await event.answer(crackexy, cache_time=0, alert=True)
             out_file = reply_pop_up_alert
