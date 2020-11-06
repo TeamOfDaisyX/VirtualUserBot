c86dd0a4b7ec5be102a2b9b1bec86776ae285d59
Starkgangs <Starkgangz@gmail.com>
2020-11-06 11:32:27 +0000

    pylint: auto fixes

--- fridaybot/modules/_inlinebot.py
+++ fridaybot/modules/_inlinebot.py
@@ -130,7 +130,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 r = requests.post(url, data=out_file.encode("UTF-8")).json()
                 url = f"https://del.dog/{r['key']}"
                 await borg.send_message(event.chat_Id, f"Dogged {plugin_name} to {url}")
-                
+
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"terminator")))
     async def rip(event):
         if event.query.user_id == bot.uid:
