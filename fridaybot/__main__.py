e0b8dc02936d78ffd82345194d2fbc35879af8cd
Starkgangs <Starkgangz@gmail.com>
2020-11-09 07:37:35 +0000

    pylint: auto fixes

--- fridaybot/__main__.py
+++ fridaybot/__main__.py
@@ -2,8 +2,9 @@ from pathlib import Path
 from sys import argv
 
 import telethon.utils
-from telethon import TelegramClient
 from Configs import Config
+from telethon import TelegramClient
+
 from fridaybot import bot
 from fridaybot.utils import load_module, start_assistant
 from var import Var
