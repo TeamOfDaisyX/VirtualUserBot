bf78f0d1e0a6360f2d2bfa46b75fcdf448fe8201
StarkGang <StarkGangZ@gmail.com>
2020-11-11 15:29:03 +0530

    Update memify.py

--- fridaybot/modules/memify.py
+++ fridaybot/modules/memify.py
@@ -15,6 +15,7 @@ thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 
 @friday.on(friday_on_cmd("mmf ?(.*)"))
 async def _(event):
+    hmm = event.chat_id
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
@@ -43,9 +44,10 @@ async def _(event):
             await silently_send_message(bot_conv, "/create")
             await asyncio.sleep(1)
             await borg.send_file(chat, reply_message.media)
-            await asyncio.sleep(1)
+            await asyncio.sleep(3)
             await silently_send_message(bot_conv, memeVar)
             response = await bot_conv.get_response()
+            await borg.send_message(hmm, response.media)
         except YouBlockedUserError:
             await event.reply("```Please unblock @TgMemeRobot and try again```")
             return
@@ -57,60 +59,7 @@ async def _(event):
             await event.edit(
                 "```🤨 NANI?! This is not an image! This will take sum tym to convert to image owo 🧐```"
             )
-            thumb = None
-            if os.path.exists(thumb_image_path):
-                thumb = thumb_image_path
-            input_str = event.pattern_match.group(1)
-            if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-                os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-            if event.reply_to_msg_id:
-                file_name = "meme.png"
-                reply_message = await event.get_reply_message()
-                to_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
-                downloaded_file_name = os.path.join(to_download_directory, file_name)
-                downloaded_file_name = await borg.download_media(
-                    reply_message,
-                    downloaded_file_name,
-                )
-                if os.path.exists(downloaded_file_name):
-                    await borg.send_file(
-                        chat,
-                        downloaded_file_name,
-                        force_document=False,
-                        supports_streaming=False,
-                        allow_cache=False,
-                        thumb=thumb,
-                    )
-                    os.remove(downloaded_file_name)
-                else:
-                    await event.edit("File Not Found {}".format(input_str))
-            response = await bot_conv.get_response()
-            the_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
-            files_name = "memes.webp"
-            download_file_name = os.path.join(the_download_directory, files_name)
-            await borg.download_media(
-                response.media,
-                download_file_name,
-            )
-            requires_file_name = Config.TMP_DOWNLOAD_DIRECTORY + "memes.webp"
-            await borg.send_file(  # pylint:disable=E0602
-                event.chat_id,
-                requires_file_name,
-                supports_streaming=False,
-                caption="@FridayOT",
-            )
-            await event.delete()
-            sax = await borg.send_message(event.chat_id, "Memeify Sucess !")
-            await asyncio.sleep(4)
-            sax.delete()
-        elif not is_message_image(reply_message):
-            await event.edit(
-                "Invalid message type. Plz choose right message type u NIBBA."
-            )
-            return
-        else:
-            await borg.send_file(event.chat_id, response.media)
-
+            
 
 def is_message_image(message):
     if message.media:
