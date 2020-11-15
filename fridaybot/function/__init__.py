c5d5cf3d26148f8c96f39ac537b04b7849a7333f
StarkGang <StarkGangZ@gmail.com>
2020-11-15 14:48:01 +0530

    Update __init__.py

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -1,3 +1,8 @@
-# Copyright (C) Is Distributed Between @StarkGang And @ZeltraxRockz
-# Please Ask At @FridayOT Before Copying Any Module
-# FridayUserbot (2020-21)
+# Api(s)
+jiobase = ("https://www.jiosaavn.com/api.php?__call=autocomplete.get&_format=json&_marker=0&cc=in&includeMetaTags=1&query=")
+# https://github.com/Cyberboysumanjay/apis
+jiocbs = "http://starkmusic.herokuapp.com/result/?query="
+torrents = "https://api.sumanjay.cf/torrent/?query="
+webshot = "https://api.sumanjay.cf/ss/?url="
+covid_global = "https://api.sumanjay.cf/covid/?country="
+ekart = "https://api.sumanjay.cf/ekart/"
