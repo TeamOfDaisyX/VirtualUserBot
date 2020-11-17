fcee281bb136f44ecaa7e3e011068f22f0b83fce
Starkgangs <Starkgangz@gmail.com>
2020-11-17 07:06:14 +0000

    pylint: auto fixes

--- resources/string_gen.py
+++ resources/string_gen.py
@@ -19,8 +19,7 @@ API_HASH = input("Enter API HASH here: \n")
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     try:
         session = client.session.save()
-        client.send_message("me",
-                            f"String Session \nTap To Copy. \n`{session}`")
+        client.send_message("me", f"String Session \nTap To Copy. \n`{session}`")
         print("String Generated Sucessfully Check Your Saved Message.")
     except Exception as sed:
         print(f"Something Went Wrong While Generating String \nError : {sed}")
