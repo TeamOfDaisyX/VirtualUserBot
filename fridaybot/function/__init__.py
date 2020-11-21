1d014efb45689c850b007319c04f241089668a76
Starkgangs <Starkgangz@gmail.com>
2020-11-21 14:00:15 +0000

    pylint: auto fixes

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -9,6 +9,8 @@ ekart = "https://api.sumanjay.cf/ekart/"
 # https://github.com/Nirmalraj10567/zee5-dl-bot/
 token_url1 = "https://useraction.zee5.com/tokennd"
 search_api_endpoint = "https://gwapi.zee5.com/content/details/"
-platform_token = "https://useraction.zee5.com/token/platform_tokens.php?platform_name=web_app"
+platform_token = (
+    "https://useraction.zee5.com/token/platform_tokens.php?platform_name=web_app"
+)
 token_url2 = "https://useraction.zee5.com/token"
 stream_baseurl = "https://zee5vodnd.akamaized.net"
