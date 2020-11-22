144011f7f2f3167c256747971471a702490bbe62
Starkgangs <Starkgangz@gmail.com>
2020-11-22 09:19:29 +0000

    pylint: auto fixes

--- fridaybot/modules/imagetools.py
+++ fridaybot/modules/imagetools.py
@@ -1,11 +1,15 @@
 import os
+
 import requests
-from fridaybot.utils import friday_on_cmd, sudo_cmd
 from telethon.tl.types import MessageMediaPhoto
+
+from fridaybot.utils import friday_on_cmd, sudo_cmd
+
 sedpath = "./starkgangz/"
 if not os.path.isdir(sedpath):
     os.makedirs(sedpath)
- 
+
+
 @friday.on(friday_on_cmd(pattern=r"cit"))
 @friday.on(sudo_cmd(pattern=r"cit", allow_sudo=True))
 async def hmm(event):
@@ -16,9 +20,7 @@ async def hmm(event):
     if not event.reply_to_msg_id:
         await event.reply("Reply to any Image.")
         return
-    headers = {
-    "api-key": life
-    }
+    headers = {"api-key": life}
     hmm = await event.edit("Colourzing..")
     sed = await event.get_reply_message()
     if isinstance(sed.media, MessageMediaPhoto):
@@ -29,13 +31,12 @@ async def hmm(event):
         await event.edit("Reply To Image")
         return
     img_file = {
-    "image": open(img, "rb"),
+        "image": open(img, "rb"),
     }
     url = "https://api.deepai.org/api/colorizer"
     r = requests.post(url=url, files=img_file, headers=headers).json()
-    sedimg = r['output_url']
+    sedimg = r["output_url"]
     await borg.send_file(event.chat_id, sedimg)
     await hmm.delete()
     if os.path.exists(img):
         os.remove(img)
-    
