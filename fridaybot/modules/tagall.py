0b6484d991363cd283d6909b98d8465ce62746f0
Starkgangs <Starkgangz@gmail.com>
2020-11-22 08:07:48 +0000

    pylint: auto fixes

--- fridaybot/modules/tagall.py
+++ fridaybot/modules/tagall.py
@@ -2,7 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from uniborg.util import friday_on_cmd
+
 from fridaybot import CMD_HELP
+
 
 @friday.on(friday_on_cmd(pattern="tagall"))
 async def _(event):
