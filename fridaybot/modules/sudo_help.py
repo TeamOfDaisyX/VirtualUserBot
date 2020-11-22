073a1e3a29fc441b7a929402949bfb10391e6475
Starkgangs <Starkgangz@gmail.com>
2020-11-22 04:50:47 +0000

    pylint: auto fixes

--- fridaybot/modules/sudo_help.py
+++ fridaybot/modules/sudo_help.py
@@ -1,5 +1,5 @@
-from fridaybot import SUDO_LIST
 from userbot import CMD_HELP
+
 from fridaybot.utils import sudo_cmd
 
 
@@ -10,7 +10,8 @@ async def _(event):
         if args in CMD_HELP:
             await event.edit(
                 f"Here is some help for the **{CMD_HELP[args][0]}** module:\n\n"
-                + str(CMD_HELP[args][1]))
+                + str(CMD_HELP[args][1])
+            )
         else:
             await event.edit(
                 f"Help string for {args} not found! Type ```.help``` to see valid module names."
@@ -20,5 +21,6 @@ async def _(event):
         for i in CMD_HELP.values():
             string += f"`{str(i[0])}`, "
         string = string[:-2]
-        await event.edit("Please specify which module you want help for!\n\n"
-                         f"{string}")
+        await event.edit(
+            "Please specify which module you want help for!\n\n" f"{string}"
+        )
