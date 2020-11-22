06a71cdc023af9ce9c040d6a49f1025be6a0b9ae
StarkGang <StarkGangZ@gmail.com>
2020-11-22 15:08:26 +0530

    hmm

--- fridaybot/modules/imagetools.py
+++ fridaybot/modules/imagetools.py
@@ -1,3 +1,16 @@
+# Api Provided By @PythonvsAno
+#    Copyright (C) Midhun KM 2020
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import os
 
 import requests
@@ -35,8 +48,83 @@ async def hmm(event):
     }
     url = "https://api.deepai.org/api/colorizer"
     r = requests.post(url=url, files=img_file, headers=headers).json()
+    if r['err']:
+        await event.edit("Some Errors. \nOutput : r['err']")
+        return
     sedimg = r["output_url"]
     await borg.send_file(event.chat_id, sedimg)
     await hmm.delete()
     if os.path.exists(img):
         os.remove(img)
+        
+@friday.on(friday_on_cmd(pattern=r"toon"))
+@friday.on(sudo_cmd(pattern=r"toon", allow_sudo=True))
+async def hmm(event):
+    life = Config.DEEP_API_KEY
+    if life == None:
+        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+        await event.edit("No Api Key Found, Please Add it. For Now Using Local Key")
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    headers = {"api-key": life}
+    hmm = await event.edit("Colourzing..")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in response.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    img_file = {
+        "image": open(img, "rb"),
+    }
+    url = "https://api.deepai.org/api/toonify"
+    r = requests.post(url=url, files=img_file, headers=headers).json()
+    if r['err']:
+        await event.edit("Some Errors. \nOutput : r['err']")
+        return
+    sedimg = r["output_url"]
+    await borg.send_file(event.chat_id, sedimg)
+    await hmm.delete()
+    if os.path.exists(img):
+        os.remove(img)
+        
+@friday.on(friday_on_cmd(pattern=r"nst"))
+@friday.on(sudo_cmd(pattern=r"nst", allow_sudo=True))
+async def hmm(event):
+    life = Config.DEEP_API_KEY
+    if life == None:
+        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+        await event.edit("No Api Key Found, Please Add it. For Now Using Local Key")
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    headers = {"api-key": life}
+    hmm = await event.edit("Colourzing..")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in response.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    img_file = {
+        "image": open(img, "rb"),
+    }
+    url = "https://api.deepai.org/api/nsfw-detector"
+    r = requests.post(url=url, files=img_file, headers=headers).json()
+    if r['err']:
+        await event.edit("Some Errors. \nOutput : r['err']")
+        return
+    sedcopy = r["output"]
+    hmmyes = sedcopy['detections']
+    game = sedcopy['nsfw_score'] 
+    final = (f"**IMG RESULT** \n**Detections :** `{hmmyes}` \n**NSFW SCORE :** `{game}`")
+    await borg.send_message(event.chat_id, final)
+    await hmm.delete()
+    if os.path.exists(img):
+        os.remove(img)
+        
