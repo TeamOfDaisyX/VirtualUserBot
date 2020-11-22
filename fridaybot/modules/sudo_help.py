27e6318f9b3164a1d6f0fb043980ddfefa84b57b
StarkGang <StarkGangZ@gmail.com>
2020-11-22 10:20:04 +0530

    Testing Paperplanes help module

--- fridaybot/modules/sudo_help.py
+++ fridaybot/modules/sudo_help.py
@@ -1,17 +1,24 @@
 from fridaybot import SUDO_LIST
+from userbot import CMD_HELP
 from fridaybot.utils import sudo_cmd
 
 
 @friday.on(sudo_cmd(pattern="help ?(.*)", allow_sudo=True))
 async def _(event):
-    if event.fwd_from:
-        return
-    plugin_name = event.pattern_match.group(1)
-    help_string = "**SUDO COMMANDS** \n"
-    try:
-        for i in SUDO_LIST[plugin_name]:
-            help_string += f"$× `{i}`"
-            help_string += "\n"
-        await event.reply(f"{help_string}")
-    except:
-        await event.reply(f"`{plugin_name}` __May Not Have Sudo Or Is Not Valid__")
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(
+                f"Here is some help for the **{CMD_HELP[args][0]}** module:\n\n"
+                + str(CMD_HELP[args][1]))
+        else:
+            await event.edit(
+                f"Help string for {args} not found! Type ```.help``` to see valid module names."
+            )
+    else:
+        string = ""
+        for i in CMD_HELP.values():
+            string += f"`{str(i[0])}`, "
+        string = string[:-2]
+        await event.edit("Please specify which module you want help for!\n\n"
+                         f"{string}")
