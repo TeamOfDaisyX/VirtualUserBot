d99836d4b7f8a9c4bf96ea7466e3023bbe20f13d
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-23 13:22:15 +0530

    Update 4.0

--- fridaybot/modules/gban_gmute.py
+++ fridaybot/modules/gban_gmute.py
@@ -1,7 +1,7 @@
 from telethon.events import ChatAction
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
 from telethon.tl.types import MessageEntityMentionName
-
+from fridaybot import CMD_HELP
 from fridaybot.utils import friday_on_cmd
 
 
@@ -204,3 +204,15 @@ async def handler(rkG):
                         except:
                             rkG.reply("`No Permission To Ban`")
                             return
+
+
+
+CMD_HELP.update(
+    {
+        "gban_gmute": "**Gban_Gmute**\
+\n\n**Syntax : **`.gban <reply to a user>`\
+\n**Usage :** bans the user in every group where you are admin.\
+\n\n**Syntax : **`.gmute <reply to a user>`\
+\n**Usage :** mutes the user in every group where you are admin."
+    }
+)
