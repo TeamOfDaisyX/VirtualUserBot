217abd572102e5ad481318f8b236de970c033946
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-23 09:38:17 +0530

    Update 4.0

--- fridaybot/modules/lyric.py
+++ fridaybot/modules/lyric.py
@@ -108,16 +108,14 @@ async def lyrics(lyric):
         )
     return
 
-
 CMD_HELP.update(
     {
-        "lyrics": ".lyrics song name \
-    \n USAGE: searches a song lyrics and sends you if song name doesnt work try along with artisyt name"
-        "**Usage:** .`glyrics <artist name> - <song name>`\n"
-        "__note__: **-** is neccessary when searching the lyrics to divided artist and song \n"
-        "Genius lyrics plugin \n"
-        "get this value from https://genius.com/developers \n"
-        "Add:-  GENIUS_API_TOKEN and token value in heroku app settings \n"
-        "Lyrics Plugin Syntax: .lyrics <aritst name - song nane>"
+        "lyric": "**Lyric**\
+\n\n**Syntax : **`.lyrics <song name>`\
+\n**Usage :** this plugin searches for song lyrics with song name. \
+\n\n**Syntax : **`.glyrics <artist - song>`\
+\n**Usage :** this plugin searches for song lyrics with song name and artist."
     }
 )
+
+
