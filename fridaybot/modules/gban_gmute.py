9f9001ff5a95647005dfa449ae608c79214e28a6
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-23 13:33:49 +0530

    Update 4.0

--- fridaybot/modules/gban_gmute.py
+++ fridaybot/modules/gban_gmute.py
@@ -210,9 +210,9 @@ async def handler(rkG):
 CMD_HELP.update(
     {
         "gban_gmute": "**Gban_Gmute**\
-\n\n**Syntax : **`.gban <reply to a user>`\
+\n\n**Syntax : **`.gban <reply to a user / mention their ID>`\
 \n**Usage :** bans the user in every group where you are admin.\
-\n\n**Syntax : **`.gmute <reply to a user>`\
-\n**Usage :** mutes the user in every group where you are admin."
+\n\n**Syntax : **`.ungban <reply to a user / mention their ID>`\
+\n**Usage :** unbans the user in every group where you are admin."
     }
 )
