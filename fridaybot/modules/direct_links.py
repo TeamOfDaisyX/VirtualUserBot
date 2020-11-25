f0c0ba98642d60d6e30fbf1fc4ad58e8fa684ce2
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-25 20:12:27 +0530

    Update 4.0

--- fridaybot/modules/direct_links.py
+++ fridaybot/modules/direct_links.py
@@ -336,3 +336,13 @@ CMD_HELP.update(
         "ZippyShare - MediaFire - SourceForge - OSDN - GitHub`"
     }
 )
+
+
+CMD_HELP.update(
+    {
+        "direct": "**Direct**\
+\n\n**Syntax : **`.direct <url> <url>`\
+\n**Usage :** Generates direct download link from supported URL(s)\
+\n\n**Supported websites : **`Google Drive - MEGA.nz - Cloud Mail - Yandex.Disk - AFH - ZippyShare - MediaFire - SourceForge - OSDN - GitHub`
+    }
+)
