541368de57a17b2a5c8b551b9acba4d3e1b35a40
Starkgangs <Starkgangz@gmail.com>
2020-11-26 03:49:28 +0000

    pylint: auto fixes

--- fridaybot/modules/eye.py
+++ fridaybot/modules/eye.py
@@ -3,8 +3,9 @@
 
 import asyncio
 
-from fridaybot.utils import friday_on_cmd
 from fridaybot import CMD_HELP
+from fridaybot.utils import friday_on_cmd
+
 
 @friday.on(friday_on_cmd(pattern="eye"))
 async def _(event):
