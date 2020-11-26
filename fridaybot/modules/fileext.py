09125b22e7951bb65debf1dce97ed0fa0571de9b
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-26 11:27:56 +0530

    Update 4.0 fileext.py

--- fridaybot/modules/fileext.py
+++ fridaybot/modules/fileext.py
@@ -3,7 +3,7 @@ Syntax: .filext EXTENSION"""
 import requests
 from bs4 import BeautifulSoup
 from uniborg.util import friday_on_cmd
-
+from fridaybot import CMD_HELP
 
 @friday.on(friday_on_cmd(pattern="filext (.*)"))
 async def _(event):
@@ -29,3 +29,12 @@ async def _(event):
                 status_code, input_str
             )
         )
+
+
+CMD_HELP.update(
+    {
+        "fileext": "**File Extension**\
+\n\n**Syntax : **`.filext <extension>`\
+\n**Usage :** Gives details about the extension."
+    }
+)
