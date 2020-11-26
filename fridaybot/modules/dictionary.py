eb70171782f11416b949df5b433114d0581dff41
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-26 10:30:23 +0530

    Update 4.0 dictionary.py

--- fridaybot/modules/dictionary.py
+++ fridaybot/modules/dictionary.py
@@ -3,7 +3,7 @@ Syntax: .meaning <word>"""
 
 import requests
 from uniborg.util import edit_or_reply, friday_on_cmd, sudo_cmd
-
+from fridaybot import CMD_HELP
 
 @friday.on(friday_on_cmd("meaning (.*)"))
 @friday.on(sudo_cmd("meaning (.*)", allow_sudo=True))
@@ -45,3 +45,12 @@ async def _(event):
     except:
         pass
     await stark.edit(caption_str)
+
+
+CMD_HELP.update(
+    {
+        "dictionary": "**Dictionary**\
+\n\n**Syntax : **`.meaning <word>`\
+\n**Usage :** Get meaning and pronunciation of a word."
+    }
+)
