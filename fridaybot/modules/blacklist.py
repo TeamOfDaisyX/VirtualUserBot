f430b4474aadf89dae3274162f50fe8a78d02e96
Starkgangs <Starkgangz@gmail.com>
2020-11-26 03:19:45 +0000

    pylint: auto fixes

--- fridaybot/modules/blacklist.py
+++ fridaybot/modules/blacklist.py
@@ -11,8 +11,9 @@ import re
 from telethon import events
 
 import fridaybot.modules.sql_helper.blacklist_sql as sql
-from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
 from fridaybot import CMD_HELP
+from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
+
 
 @friday.on(events.NewMessage(incoming=True))
 async def on_new_message(event):
