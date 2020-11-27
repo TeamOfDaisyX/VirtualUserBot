3a22e36a89318d18032b00c50d1c36cf86bfde1b
Starkgangs <Starkgangz@gmail.com>
2020-11-27 10:40:40 +0000

    pylint: auto fixes

--- fridaybot/modules/jainder.py
+++ fridaybot/modules/jainder.py
@@ -4,7 +4,9 @@
 import random
 
 from telethon import events
+
 from fridaybot import CMD_HELP
+
 
 @friday.on(events.NewMessage(pattern=r"\.jainder(.*)", outgoing=True))
 async def _(event):
@@ -103,7 +105,6 @@ async def _(event):
     index = random.randint(0, len(emoticons))
     output_str = emoticons[index]
     await event.edit(output_str)
-
 
 
 CMD_HELP.update(
