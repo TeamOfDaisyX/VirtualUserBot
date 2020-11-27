c33354f9f9ee2b0b2db75785de00196959111cda
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-27 21:27:46 +0530

    Password strength checker

---
+++ fridaybot/modules/password_check.py
@@ -0,0 +1,35 @@
+#plugin by @chsaiujwal
+#plugin by @chsaiujwal
+#plugin by @chsaiujwal
+#plugin by @chsaiujwal
+#plugin by @chsaiujwal
+
+
+from telethon import events
+from fridaybot import CMD_HELP
+from password_strength import PasswordStats, PasswordPolicy
+
+
+@friday.on(events.NewMessage(pattern=r"\.passcheck(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    stats = PasswordStats(input_str)
+    stats.strength()
+	
+    if stats.strength()>=0.6:
+      await event.edit("Good Password. 😎")
+    else:
+      await event.edit("bad password, change it.😔")
+  
+  
+  
+  
+CMD_HELP.update(
+    {
+        "passcheck": "**Password Checker**\
+\n\n**Syntax : **`.passcheck <your password>`\
+\n**Usage :** This plugin checks the strength of your password."
+    }
+)
