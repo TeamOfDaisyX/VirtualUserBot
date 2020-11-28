d6769845600506d47e9a641791ea91da049fdb72
Starkgangs <Starkgangz@gmail.com>
2020-11-28 01:46:17 +0000

    pylint: auto fixes

--- fridaybot/modules/password_check.py
+++ fridaybot/modules/password_check.py
@@ -1,13 +1,14 @@
-#plugin by @chsaiujwal
-#plugin by @chsaiujwal
-#plugin by @chsaiujwal
-#plugin by @chsaiujwal
-#plugin by @chsaiujwal
+# plugin by @chsaiujwal
+# plugin by @chsaiujwal
+# plugin by @chsaiujwal
+# plugin by @chsaiujwal
+# plugin by @chsaiujwal
 
 
+from password_strength import PasswordStats
 from telethon import events
+
 from fridaybot import CMD_HELP
-from password_strength import PasswordStats, PasswordPolicy
 
 
 @friday.on(events.NewMessage(pattern=r"\.passcheck(.*)", outgoing=True))
@@ -17,15 +18,13 @@ async def _(event):
     input_str = event.pattern_match.group(1)
     stats = PasswordStats(input_str)
     stats.strength()
-	
-    if stats.strength()>=0.6:
-      await event.edit("Good Password. 😎")
+
+    if stats.strength() >= 0.6:
+        await event.edit("Good Password. 😎")
     else:
-      await event.edit("bad password, change it.😔")
-  
-  
-  
-  
+        await event.edit("bad password, change it.😔")
+
+
 CMD_HELP.update(
     {
         "passcheck": "**Password Checker**\
