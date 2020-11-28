0097aaf5472266688b9007df14f005f83f19ec33
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-28 11:27:25 +0530

    Update 4.0 mask.py

--- fridaybot/modules/mask.py
+++ fridaybot/modules/mask.py
@@ -1,7 +1,7 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from uniborg.util import friday_on_cmd
-
+from fridaybot import CMD_HELP
 
 @friday.on(friday_on_cmd("mask ?(.*)"))
 async def _(event):
@@ -36,3 +36,13 @@ async def _(event):
             )
         else:
             await borg.send_file(event.chat_id, response.message.media)
+
+
+
+CMD_HELP.update(
+    {
+        "mask": "**Mask**\
+\n\n**Syntax : **`.mask <reply to image>`\
+\n**Usage :** This funny plugin masks the image."
+    }
+)
