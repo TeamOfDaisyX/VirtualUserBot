dcac694d6e98542d0c91d9bb2bba8c5e6597499d
Starkgangs <Starkgangz@gmail.com>
2020-11-28 15:18:23 +0000

    pylint: auto fixes

--- fridaybot/modules/nakal.py
+++ fridaybot/modules/nakal.py
@@ -13,7 +13,9 @@ Available Commands:
 import asyncio
 
 from telethon import events
+
 from fridaybot import CMD_HELP
+
 
 @friday.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
@@ -46,7 +48,6 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 11])
-
 
 
 CMD_HELP.update(
