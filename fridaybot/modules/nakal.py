6fd19be024bcb0568fd23525afef70222bf40c79
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-28 20:47:43 +0530

    Update 🐕

--- fridaybot/modules/nakal.py
+++ fridaybot/modules/nakal.py
@@ -13,7 +13,7 @@ Available Commands:
 import asyncio
 
 from telethon import events
-
+from fridaybot import CMD_HELP
 
 @friday.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
@@ -46,3 +46,13 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 11])
+
+
+
+CMD_HELP.update(
+    {
+        "nakal": "**nakal**\
+\n\n**Syntax : **`.nakal`\
+\n**Usage :** funny plugin to abuse someone."
+    }
+)
