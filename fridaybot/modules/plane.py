5d5acc20d1a1caaec85b898629910e57ee08a34d
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-29 12:33:19 +0530

    Update 🐕. plane.py

--- fridaybot/modules/plane.py
+++ fridaybot/modules/plane.py
@@ -2,7 +2,7 @@
 import asyncio
 
 from telethon import events
-
+from fridaybot import CMD_HELP
 
 @friday.on(events.NewMessage(pattern=r"\.plane", outgoing=True))
 async def _(event):
@@ -25,3 +25,13 @@ async def _(event):
     await event.edit("-------------✈")
     await asyncio.sleep(3)
     await event.delete()
+
+
+
+CMD_HELP.update(
+    {
+        "plane": "**Plane**\
+\n\n**Syntax : **`.plane`\
+\n**Usage :** Creates plane prank."
+    }
+)
