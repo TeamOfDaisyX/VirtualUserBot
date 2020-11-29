463f3fcee4af57fa7fa1c17ba814a2b988e32802
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-29 12:13:39 +0530

    Update 🐕. padmin.py

--- fridaybot/modules/padmin.py
+++ fridaybot/modules/padmin.py
@@ -7,7 +7,7 @@ Available Commands:
 import asyncio
 
 from telethon import events
-
+from fridaybot import CMD_HELP
 
 @friday.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
@@ -54,3 +54,13 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 20])
+
+
+
+CMD_HELP.update(
+    {
+        "padmin": "**Prank admin**\
+\n\n**Syntax : **`.padmin`\
+\n**Usage :** Pranks users promotion."
+    }
+)
