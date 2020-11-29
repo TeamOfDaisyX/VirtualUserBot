cc10f3f1933d527a1f2d8cc0af9955ecad0b1ae7
Starkgangs <Starkgangz@gmail.com>
2020-11-29 11:21:55 +0000

    pylint: auto fixes

--- fridaybot/modules/sangmata.py
+++ fridaybot/modules/sangmata.py
@@ -1,7 +1,9 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from uniborg.util import friday_on_cmd
+
 from fridaybot import CMD_HELP
+
 
 @friday.on(friday_on_cmd("sg ?(.*)"))
 async def _(event):
@@ -177,8 +179,6 @@ async def _(event):
             )
         else:
             await event.edit(f"{response.message.message}")
-
-
 
 
 CMD_HELP.update(
