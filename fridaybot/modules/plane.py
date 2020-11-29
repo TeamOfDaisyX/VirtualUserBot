f06b9a034859a8cbe84f805c32d35c8f0b5b0dde
Starkgangs <Starkgangz@gmail.com>
2020-11-29 07:03:56 +0000

    pylint: auto fixes

--- fridaybot/modules/plane.py
+++ fridaybot/modules/plane.py
@@ -2,7 +2,9 @@
 import asyncio
 
 from telethon import events
+
 from fridaybot import CMD_HELP
+
 
 @friday.on(events.NewMessage(pattern=r"\.plane", outgoing=True))
 async def _(event):
@@ -25,7 +27,6 @@ async def _(event):
     await event.edit("-------------✈")
     await asyncio.sleep(3)
     await event.delete()
-
 
 
 CMD_HELP.update(
