a33fa641da5931cd0799a8c77f301b520cf711bd
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-29 11:41:04 +0530

    Update nsfw.py 🐕

--- fridaybot/modules/nsfw.py
+++ fridaybot/modules/nsfw.py
@@ -14,7 +14,7 @@ import os
 
 import requests
 from telethon.tl.types import MessageMediaPhoto
-
+from fridaybot import CMD_HELP
 from fridaybot.utils import friday_on_cmd, sudo_cmd
 
 
@@ -46,3 +46,13 @@ async def nsfw(event):
             await event.edit("Response UnsucessFull. Try Again.")
             if os.path.exists(photo):
                 os.remove(photo)
+
+
+
+CMD_HELP.update(
+    {
+        "nsfw": "**NSFW**\
+\n\n**Syntax : **`.nsfw <reply to image>`\
+\n**Usage :** Checks if the replyed image is nsfw or not."
+    }
+)
