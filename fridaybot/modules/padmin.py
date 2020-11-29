4dc1c9a0f4dd8975a4d1a27b5ec6438ab299de10
Starkgangs <Starkgangz@gmail.com>
2020-11-29 06:44:20 +0000

    pylint: auto fixes

--- fridaybot/modules/padmin.py
+++ fridaybot/modules/padmin.py
@@ -7,7 +7,9 @@ Available Commands:
 import asyncio
 
 from telethon import events
+
 from fridaybot import CMD_HELP
+
 
 @friday.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
@@ -54,7 +56,6 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 20])
-
 
 
 CMD_HELP.update(
