22903bcc4c5d0bc5f182132f8dbd8ab07cb9da24
Starkgangs <Starkgangz@gmail.com>
2020-11-29 10:58:55 +0000

    pylint: auto fixes

--- fridaybot/modules/rename.py
+++ fridaybot/modules/rename.py
@@ -8,6 +8,7 @@ import time
 from datetime import datetime
 
 from uniborg.util import friday_on_cmd
+
 from fridaybot import CMD_HELP
 
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
@@ -59,8 +60,6 @@ async def _(event):
             await event.edit("File Not Found {}".format(input_str))
     else:
         await event.edit("Syntax // .rnupload file.name as reply to a Telegram media")
-
-
 
 
 CMD_HELP.update(
