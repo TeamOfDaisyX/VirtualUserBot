a43fe61ed5c8df7aedc58661e1293547ba666a07
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-30 10:07:49 +0530

    Update 🐕. schd.py

--- fridaybot/modules/schd.py
+++ fridaybot/modules/schd.py
@@ -3,7 +3,7 @@ Syntax: .schd <time_in_seconds> ;=; <message to send>"""
 import asyncio
 
 from fridaybot.utils import friday_on_cmd
-
+from fridaybot import CMD_HELP
 
 @friday.on(friday_on_cmd("schd ?(.*)"))
 async def _(event):
@@ -24,3 +24,14 @@ async def _(event):
         await event.respond(message)
     else:
         await event.edit(message)
+
+
+
+
+CMD_HELP.update(
+    {
+        "schd": "**Schedule message**\
+\n\n**Syntax : **`.schd <time in seconds> = <message to send>`\
+\n**Usage :** given message is send automatically after given time."
+    }
+)
