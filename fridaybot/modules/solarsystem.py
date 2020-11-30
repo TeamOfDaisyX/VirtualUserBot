b4498614a3ce55e23badac417d8a88843c6a38fe
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-11-30 19:58:56 +0530

    Update 🐕. Solarsystem.py

--- fridaybot/modules/solarsystem.py
+++ fridaybot/modules/solarsystem.py
@@ -8,7 +8,7 @@ Available Commands:
 import asyncio
 
 from telethon import events
-
+from fridaybot import CMD_HELP
 
 @friday.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
@@ -169,3 +169,14 @@ async def _(event):
         for i in animation_ttl:
             await asyncio.sleep(animation_interval)
             await event.edit(animation_chars[i % 549755813888])
+
+
+
+
+CMD_HELP.update(
+    {
+        "solarsystem": "**Solar System**\
+\n\n**Syntax : **`.solarsystem`\
+\n**Usage :** creates solar system with text."
+    }
+)
