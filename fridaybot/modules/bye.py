23b42c7993cc8a1f8509c961f445677f414764ee
StarkGang <StarkGangZ@gmail.com>
2020-12-03 18:34:22 +0530

    Fix

--- fridaybot/modules/bye.py
+++ fridaybot/modules/bye.py
@@ -12,17 +12,16 @@ from fridaybot import CMD_HELP
 from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
 
 
-@friday.on(friday_on_cmd("bye", outgoing=True))
+@friday.on(friday_on_cmd("bye"))
 @friday.on(sudo_cmd("bye", allow_sudo=True))
 async def leave(e):
-    starkgang = await edit_or_reply(e, "Bye Kek")
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await starkgang.edit("`I am leaving this chat.....!`")
         time.sleep(3)
-        if "-" in str(e.chat_id):
+        if e.is_group:
+            await starkgang.edit("`This is Very kensur Group, So Me iz Leaving.`")
             await borg(LeaveChannelRequest(e.chat_id))
         else:
-            await starkgang.edit("`Sir This is Not A Chat`")
+            await starkgang.edit("`Boss, This is Not A Chat`")
 
 
 CMD_HELP.update(
