cadb7377dc8b74c5b908ae86b5bf423530d5faa9
StarkGang <StarkGangZ@gmail.com>
2020-12-04 13:13:30 +0530

    Create ccchecker.py

---
+++ fridaybot/modules/ccchecker.py
@@ -0,0 +1,19 @@
+from resources.cc_alogo import CreditCard
+from fridaybot.utils import admin_cmd
+
+@friday.on(admin_cmd(pattern='cccheck'))
+async def lol(event):
+    cnn = event.pattern_match.group(1)
+    if ccn.isnumeric():
+      card_number = int(ccn)
+    else:
+      event.edit('Card Number Should Be Only Number')
+      return
+    card = CreditCard.set_card(card_number)
+    sed = f"""{card.company}
+Card : {card.card_no}
+{card.first_check()}
+{card.checksum}
+{card.validate()}
+"""
+    await event.edit(sed)
