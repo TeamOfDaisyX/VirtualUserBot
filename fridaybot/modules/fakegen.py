bc0341655f659cdf3b54045aad6bd8b0268ee016
Starkgangs <Starkgangz@gmail.com>
2020-12-04 15:04:00 +0000

    pylint: auto fixes

--- fridaybot/modules/fakegen.py
+++ fridaybot/modules/fakegen.py
@@ -1,9 +1,9 @@
 from faker import Faker
 from faker.providers import internet
 from uniborg.util import friday_on_cmd
-from asyncio import wait
+
 from fridaybot import CMD_HELP
-from telethon import events
+
 
 @friday.on(friday_on_cmd(pattern=r"fakegen"))
 async def hi(event):
@@ -21,8 +21,9 @@ async def hi(event):
     android = fake.android_platform_token()
     pc = fake.chrome()
     await event.edit(
-      f"<b><u> Fake Information Generated</b></u>\n<b>Name :-</b><code>{name}</code>\n\n<b>Address:-</b><code>{address}</code>\n\n<b>IP ADDRESS:-</b><code>{ip}</code>\n\n<b>credit card:-</b><code>{cc}</code>\n\n<b>Email Id:-</b><code>{email}</code>\n\n<b>Job:-</b><code>{job}</code>\n\n<b>android user agent:-</b><code>{android}</code>\n\n<b>Pc user agent:-</b><code>{pc}</code>", parse_mode="HTML",)
-
+        f"<b><u> Fake Information Generated</b></u>\n<b>Name :-</b><code>{name}</code>\n\n<b>Address:-</b><code>{address}</code>\n\n<b>IP ADDRESS:-</b><code>{ip}</code>\n\n<b>credit card:-</b><code>{cc}</code>\n\n<b>Email Id:-</b><code>{email}</code>\n\n<b>Job:-</b><code>{job}</code>\n\n<b>android user agent:-</b><code>{android}</code>\n\n<b>Pc user agent:-</b><code>{pc}</code>",
+        parse_mode="HTML",
+    )
 
 
 CMD_HELP.update(
