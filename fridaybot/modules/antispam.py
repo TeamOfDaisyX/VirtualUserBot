b411e5dfbc709beea8bcf3ccf042df9c2e8aa547
StarkGang <StarkGangZ@gmail.com>
2020-12-06 08:25:44 +0530

    Update antispam.py

--- fridaybot/modules/antispam.py
+++ fridaybot/modules/antispam.py
@@ -1,4 +1,5 @@
-import requests
+import antispaminc
+from fridaybot import sclient
 from telethon.events import ChatAction
 
 from fridaybot.Configs import Config
@@ -10,25 +11,14 @@ async def ok(event):
         return
     if event.user_joined or event.user_added:
         juser = await event.get_user()
-        data = {"token": Config.ANTI_SPAMINC_TOKEN, "userid": juser.id}
-        if Config.ANTI_SPAMINC_TOKEN == None:
-            logger.info("[Warning] - Your Token is None \nGet It From @AntispamIncbot")
-            return
-        url = f"http://antispaminc.tk/info/"
-        sed = requests.post(url=url, data=data).json()
-        if sed["error"] == True:
-            logger.info(
-                f"[Warning] - Errors, I Got Error While Detecting Antispam, Please Report This Error, \nERROR : {sed['full']}"
-            )
-            return
-        else:
-            if sed["banned"] == True:
+        user = sclient.is_banned(juser.id)
+        if user.banned == True:
                 try:
                     await borg.edit_permissions(
                         event.chat_id, juser.id, view_messages=False
                     )
                     await event.reply(
-                        f"**#FRIDAY-ANTISPAM** \n**Detected Malicious User.** \n**User-ID :** `{juser.id}`  \n**Reason :** `{sed['reason']}`"
+                        f"**#FRIDAY-ANTISPAM** \n**Detected Malicious User.** \n**User-ID :** `{juser.id}`  \n**Reason :** `{sed.reason}`"
                     )
                 except:
                     pass
