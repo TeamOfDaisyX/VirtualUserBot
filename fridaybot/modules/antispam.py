c7595cd4726741fbe66d6e511eef275fdc902662
StarkGang <StarkGangZ@gmail.com>
2020-12-06 19:28:41 +0530

    Update antispam.py

--- fridaybot/modules/antispam.py
+++ fridaybot/modules/antispam.py
@@ -3,21 +3,23 @@ from telethon.events import ChatAction
 from fridaybot import sclient
 from fridaybot.Configs import Config
 
+'''Bans Spammers/Scammer At time Of Arrival 
+If You Add Him The Bot Won't Restrict.'''
 
 @borg.on(ChatAction)
 async def ok(event):
     if Config.ANTISPAM_FEATURE != "ENABLE":
         return
-    if event.user_joined or event.user_added:
+    if event.user_joined:
         juser = await event.get_user()
         user = sclient.is_banned(juser.id)
         if user.banned == True:
+            await event.reply(
+                    f"**#FRIDAY-ANTISPAM** \n**Detected Malicious User.** \n**User-ID :** `{juser.id}`  \n**Reason :** `{user.reason}`"
+                )
             try:
                 await borg.edit_permissions(
                     event.chat_id, juser.id, view_messages=False
-                )
-                await event.reply(
-                    f"**#FRIDAY-ANTISPAM** \n**Detected Malicious User.** \n**User-ID :** `{juser.id}`  \n**Reason :** `{sed.reason}`"
                 )
             except:
                 pass
