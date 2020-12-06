92404b791a3d85565b0ed6e2e422abb9a026211e
StarkGang <StarkGangZ@gmail.com>
2020-12-06 10:04:01 +0530

    Update antispam.py

--- fridaybot/modules/antispam.py
+++ fridaybot/modules/antispam.py
@@ -1,5 +1,5 @@
 from telethon.events import ChatAction
-
+from fridaybot.utils import admin_cmd
 from fridaybot import sclient
 from fridaybot.Configs import Config
 
@@ -23,3 +23,77 @@ async def ok(event):
                 pass
         else:
             pass
+        
+@friday.on(admin_cmd(pattern='aban (.*)'))
+async def ok(event):
+    args = event.pattern_match.group(1).split(" ", 1)
+    extra = None
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user = await event.client.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif args:
+        userz = args[0]
+        if userz.isnumeric():
+            user = int(userz)
+        else:
+            await event.edit("Provide User-ID Not Username.")
+            return
+        if len(args) == 2:
+            extra = args[1]
+        if extra == None:
+            await event.edit("Reason Can't Be None")
+            return
+        elif extra == "":
+            await event.edit("Reason Can't Be None")
+            return
+        if not user:
+            await event.edit("Reply To User Or Mention a User.")
+            return
+        try:
+            gensys = sclient.ban(user, extra)
+            if gensys['error'] == True:
+                await event.edit("Error : " + gensys['full'])
+            else:
+                await borg.send_message('antispamincfed', f'/fban {user} {extra}')
+                event.edit(f'**User :** `{user}` \n**Reason :** `{extra}` \n**Banned Sucessfully !**')
+        except Exception as e
+            event.edit(e)
+            
+@friday.on(admin_cmd(pattern='aunban (.*)'))
+async def ok(event):
+    args = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user = await event.client.get_entity(previous_message.sender_id)
+    elif args:
+        userz = args
+        if userz.isnumeric():
+            user = int(userz)
+        else:
+            await event.reply("Fuck, Gib ID")
+            return
+        if not user:
+            await event.reply("Reply To User Or Mention a User.")
+            return
+        try:
+            gensys2 = sclient.unban(user, extra)
+            if gensys2['error'] == True:
+                await event.edit("Error : " + gensys2['full'])
+            else:
+                await borg.send_message('antispamincfed', f'/unfban {user} {extra}')
+                event.edit(f'**User :** `{user}` \n**Reason :** `{extra}` \n**Banned Sucessfully !**')
+        except Exception as e
+            event.edit(e)
+            
+@friday.on(admin_cmd(pattern='anewtoken'))
+async def tokens(event):
+    okbabe = secrets.token_urlsafe(16)
+    try:
+        skynet = sclient.new_token(sed_put)
+        if skynet['error'] == True:
+                await event.edit("Error : " + gensys2['full'])
+            else:
+                await event.edit(f'**New Token** \n**Token** : `{okbabe}`')
+    except Exception as e
+            event.edit(e)
