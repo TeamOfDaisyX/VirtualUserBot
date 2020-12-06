a63613a3d5904b47adca7ede351489fe509359f1
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-12-06 08:33:57 +0530

    New ifsc plugin.

--- fridaybot/modules/ifsc.py
+++ fridaybot/modules/ifsc.py
@@ -1,28 +1,45 @@
-"""Query Indian Financial System Code to get address of the relevant bank or branch
-Syntax: .ifsc rp <IFSC CODE>"""
-import json
+#    Copyright (C) @chsaiujwal 2020
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import requests
+import requests 
+import json
 from uniborg.util import friday_on_cmd
 
 from fridaybot import CMD_HELP
 
-
-@friday.on(friday_on_cmd(pattern="ifsc(.*)"))
+@friday.on(friday_on_cmd(pattern="ifsc (.*)"))
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    url = "https://ifsc.razorpay.com/{}".format(input_str)
-    r = requests.get(url)
-    if r.status_code == 200:
-        b = r.json()
-        a = json.dumps(b, sort_keys=True, indent=4)
-        # https://stackoverflow.com/a/9105132/4723940
-        await event.edit(str(a))
-    else:
-        await event.edit("`{}`: {}".format(input_str, r.text))
 
+    IFSC_Code = input_str
+
+    URL = "https://ifsc.razorpay.com/"
+
+    data = requests.get(URL+IFSC_Code).json() 
+
+    a = data["ADDRESS"]
+    b = data["CENTRE"]
+    c = data["BRANCH"]
+    d = data["CITY"]
+    e = data["STATE"]
+    f = data["BANK"]
+    g = data["BANKCODE"]
+    h = data["IFSC"]
+
+    await event.edit(
+      f"<b><u>INFORMATION GATHERED SUCCESSFULLY</b></u>\n\n<b>Bank Name :-</b><code>{f}</code>\n<b>Bank Address:- </b> <code>{a}</code>\n<b>Centre :-</b><code>{b}</code>\n<b>Branch :- </b><code>{c}</code>\n<b> City :-</b><code>{d}</code>\n<b>State:- </b> <code>{e}</code>\n<b>Bank Code :- </b><code>{g}</code>\n<b>Ifsc :-</b><code>{h}</code>", parse_mode="HTML",)
 
 CMD_HELP.update(
     {
