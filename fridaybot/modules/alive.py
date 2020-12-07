906c9676c9bb143be1aa7465e07e7e1b985a8c72
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-07 23:34:53 +0530

    Update alive.py

--- fridaybot/modules/alive.py
+++ fridaybot/modules/alive.py
@@ -41,7 +41,7 @@ def get_readable_time(seconds: int) -> str:
 uptime = get_readable_time((time.time() - Lastupdate))
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = Config.ALIVE_IMAGE
-pm_caption = "➥ **FRIDAY IS:** `ONLINE`\n\n"
+pm_caption = "➥ **I'm:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
 pm_caption += "➥ **Python:** `3.7.4` \n"
@@ -51,10 +51,10 @@ pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `{currentversion}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [StarkGang@Github](GitHub.com/StarkGang)\n"
+pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/inukaasith/userbot/blob/master/LICENSE)\n"
+pm_caption += "➥ **Copyright** : By [@InukaASiTH](GitHub.com/inukaasith)\n"
 pm_caption += "➥ **Check Stats By Doing** `.stat`. \n\n"
-pm_caption += "[🇮🇳 Deploy FridayUserbot 🇮🇳](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += "[Contact Dev](https://t.me/inukaasith)"
 
 
 @friday.on(friday_on_cmd(pattern=r"alive"))
