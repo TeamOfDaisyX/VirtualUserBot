be61879ee108e5c0e115c6d2fda4af2b7b84fb56
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-09 19:24:56 +0530

    Update hack.py

--- fridaybot/modules/hack.py
+++ fridaybot/modules/hack.py
@@ -46,11 +46,80 @@ async def _(event):
     else:
         await event.edit("U Dumb. Reply to User 🤷")
 
+        
+@friday.on(friday_on_cmd(pattern=r"thack"))        
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await event.client(GetFullUserRequest(reply_message.sender_id))
+        idd = reply_message.sender_id
+        if idd == 1035034432:
+            await edit_or_reply(
+                event, "This is My Master\nI can't hack my master's Account"
+            )
+        else:
+            event = await edit_or_reply(event, "Hacking..")
+            animation_chars = [
+                "`Connecting To Hacked Private Server...`",
+                "`Target Selected.`",
+                "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+                "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+                "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+                "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+                "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+                "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+                "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
+                "`Hacking... 100%\n█████████HACKED███████████ `",
+                f"`Targeted Account Hacked...\n\nPay 69$ To` {DEFAULTUSER} . `To Remove this hack..`",
+            ]
+            animation_interval = 3
+            animation_ttl = range(11)
+            for i in animation_ttl:
+                await asyncio.sleep(animation_interval)
+                await event.edit(animation_chars[i % 11])
+    else:
+        await edit_or_reply(event, "No User is Defined\n Can't hack account")
+@friday.on(friday_on_cmd(pattern=r"whack")) 
+ async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 2
+    animation_ttl = range(15)
+    event = await edit_or_reply(event, "wahack..")
+    animation_chars = [
+        "Looking for WhatsApp databases in targeted person...",
+        " User online: True\nTelegram access: True\nRead Storage: True ",
+        "Hacking... 0%\n[░░░░░░░░░░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 20s",
+        "Hacking... 11.07%\n[██░░░░░░░░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 18s",
+        "Hacking... 20.63%\n[███░░░░░░░░░░░░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 16s",
+        "Hacking... 34.42%\n[█████░░░░░░░░░░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 14s",
+        "Hacking... 42.17%\n[███████░░░░░░░░░░░░░]\n`Searching for databases`\nETA: 0m, 12s",
+        "Hacking... 55.30%\n[█████████░░░░░░░░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 10s",
+        "Hacking... 64.86%\n[███████████░░░░░░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 08s",
+        "Hacking... 74.02%\n[█████████████░░░░░░░]\n`Trying to Decrypt...`\nETA: 0m, 06s",
+        "Hacking... 86.21%\n[███████████████░░░░░]\n`Trying to Decrypt...`\nETA: 0m, 04s",
+        "Hacking... 93.50%\n[█████████████████░░░]\n`Decryption successful!`\nETA: 0m, 02s",
+        "Hacking... 100%\n[████████████████████]\n`Scanning file...`\nETA: 0m, 00s",
+        "Hacking complete!\nUploading file...",
+        "Targeted Account Hacked...!\n\n ✅ File has been successfully uploaded to my server.\nWhatsApp Database:\n`./DOWNLOADS/msgstore.db.crypt12`",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 15])
+       
+        
 
 CMD_HELP.update(
     {
         "hack": "**Hack**\
 \n\n**Syntax : **`.hack <reply to your friend>`\
-\n**Usage :** prank your friends with this hacking plugin"
+\n**Usage :** prank your friends with this hacking plugin\
+\n\n**Syntax : **`.thack reply to a person`\
+\n**Function : **__shows an animation of hacking progess bar__\
+\n\n**Syntax : **`.wahack reply to a person`\
+\n**Function : **__shows an animation of whatsapp account hacking to a replied person__\"
+
     }
 )
