1b768c60b3b0582089772abcf6efddd078b90067
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-09 12:25:28 +0530

    Update translate.py

--- fridaybot/modules/translate.py
+++ fridaybot/modules/translate.py
@@ -36,7 +36,7 @@ async def _(event):
         after_tr_text = translated.text
         # TODO: emojify the :
         # either here, or before translation
-        output_str = """**Translated By FridayUserbot** 
+        output_str = """**Translated By @InukaASiTH** 
          Source **( {} )**
          Translation **( {} )**
          {}""".format(
