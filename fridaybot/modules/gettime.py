6bbf558e130a23401489030385593649bbf0d490
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-12 12:43:28 +0530

    Update gettime.py

--- fridaybot/modules/gettime.py
+++ fridaybot/modules/gettime.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from fridaybot import CMD_HELP
 from fridaybot.utils import friday_on_cmd,edit_or_reply
 
-FONT_FILE_TO_USE = "Fonts/digital.ttf"
+
 
 
 @friday.on(friday_on_cmd("gettime ?(.*)"))  # pylint:disable=E0602
@@ -12,7 +12,7 @@ async def gn(event):
     if event.fwd_from:
         return
     current_time = datetime.now().strftime(
-        "CURRENT TIMEZONE \nLOCATION: Sri Lanka \nTime: %H:%M:%S \nDate: %d.%m.%y"
+        "CURRENT DATE & TIME \nLOCATION: Sri Lanka \nTime: %H:%M:%S \nDate: %d.%m.%y"
     )
     start = datetime.now()
     input_str = event.pattern_match.group(1)
