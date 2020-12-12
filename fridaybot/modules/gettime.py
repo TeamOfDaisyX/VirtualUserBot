4968badcc961a10332609b347710492774d8e43f
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-12 12:36:05 +0530

    Add files via upload

---
+++ fridaybot/modules/gettime.py
@@ -0,0 +1,28 @@
+
+from datetime import datetime
+
+from fridaybot import CMD_HELP
+from fridaybot.utils import friday_on_cmd
+
+FONT_FILE_TO_USE = "Fonts/digital.ttf"
+
+
+@friday.on(friday_on_cmd("gettime ?(.*)"))  # pylint:disable=E0602
+async def gn(event):
+    if event.fwd_from:
+        return
+    current_time = datetime.now().strftime(
+        "CURRENT TIMEZONE \nLOCATION: Sri Lanka \nTime: %H:%M:%S \nDate: %d.%m.%y"
+    )
+    start = datetime.now()
+    input_str = event.pattern_match.group(1)
+    reply_msg_id = event.message.id
+    if input_str:
+        current_time = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        reply_msg_id = previous_message.id
+    await edit_or_reply(
+        event,
+       current_time,
+    )
