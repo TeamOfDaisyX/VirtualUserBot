b6d8e50e7c7021731f1de25e8706b0d8f5027794
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-13 15:44:35 +0530

    Update dictionary.py

--- fridaybot/modules/dictionary.py
+++ fridaybot/modules/dictionary.py
@@ -1,8 +1,8 @@
-"""Dictionary Plugin for @UniBorg
-Syntax: .meaning <word>"""
+"""Syntax: .meaning <word>"""
 
 import requests
 from uniborg.util import edit_or_reply, friday_on_cmd, sudo_cmd
+from PyDictionary import PyDictionary
 
 from fridaybot import CMD_HELP
 
@@ -14,39 +14,13 @@ async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    input_url = "https://bots.shrimadhavuk.me/dictionary/?s={}".format(input_str)
-    headers = {"USER-AGENT": "UniBorg"}
-    caption_str = f"Meaning of __{input_str}__\n"
-    try:
-        response = requests.get(input_url, headers=headers).json()
-        pronounciation = response.get("p")
-        meaning_dict = response.get("lwo")
-        for current_meaning in meaning_dict:
-            current_meaning_type = current_meaning.get("type")
-            current_meaning_definition = current_meaning.get("definition")
-            caption_str += (
-                f"**{current_meaning_type}**: {current_meaning_definition}\n\n"
-            )
-    except Exception as e:
-        caption_str = str(e)
-    reply_msg_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_msg_id = event.reply_to_msg_id
-    try:
-        await borg.send_file(
-            event.chat_id,
-            pronounciation,
-            caption=f"Pronounciation of __{input_str}__",
-            force_document=False,
-            reply_to=reply_msg_id,
-            allow_cache=True,
-            voice_note=True,
-            silent=True,
-            supports_streaming=True,
-        )
-    except:
-        pass
-    await stark.edit(caption_str)
+    dictionary=PyDictionary()
+    a = (dictionary.meaning(input_str))
+    b = a.get("Noun")
+    chsaiujwal = ""
+    for x in b:
+	    chsaiujwal += x+"\n"
+    await stark.edit(f"<b> meaning of {input_str} is:-</b>\n {chsaiujwal}", parse_mode="HTML",)
 
 
 CMD_HELP.update(
