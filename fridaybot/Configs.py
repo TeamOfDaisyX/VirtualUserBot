7bcb6ac36f4188a73285d89729b59084bba16aa5
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-18 15:46:06 +0530

    Update Configs.py

--- fridaybot/Configs.py
+++ fridaybot/Configs.py
@@ -143,10 +143,10 @@ if ENV:
         ANTISPAM_FEATURE = os.environ.get("ANTISPAM_FEATURE", "ENABLE")
         ASSISTANT_LOG = int(os.environ.get("ASSISTANT_LOG", False))
         UPSTREAM_REPO = os.environ.get(
-            "UPSTREAM_REPO", "https://github.com/Inukaasith/userbot"
+            "UPSTREAM_REPO", "https://github.com/Inukaasith/VirtualUserbot"
         )
         ALIVE_IMAGE = os.environ.get(
-            "ALIVE_IMAGE", "https://telegra.ph/file/22535f8051a58af113586.jpg"
+            "ALIVE_IMAGE", "https://telegra.ph/file/b1922e21e94ba5a7a19d0.jpg"
         )
         ASSISTANT_START_PIC = os.environ.get(
             "ASSISTANT_START_PIC",
