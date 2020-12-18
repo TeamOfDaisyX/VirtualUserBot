3bafd7cc7d8e088c3f639fd20ee4f4276264ea83
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-18 23:13:28 +0530

    Update __init__.py

--- fridaybot/modules/__init__.py
+++ fridaybot/modules/__init__.py
@@ -55,7 +55,7 @@ else:
     dbstats = "Not Fine ❌"
 
 inlinestats = (
-    f"✘ SHOWING FRIDAY STATS ✘\n"
+    f"✘ SHOWING VirtualUserbot STATS ✘\n"
     f"VERSION = {currentversion} \n"
     f"DATABASE = {dbstats} \n"
     f"SUDO = {amiusingsudo} \n"
