16edfb794b1545f947c10f1ab2a397e4eede54f5
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-18 23:04:53 +0530

    Update translater.py

--- fridaybot/modules/assistant/translater.py
+++ fridaybot/modules/assistant/translater.py
@@ -36,7 +36,7 @@ async def _(event):
     translated = translator.translate(text, dest=lan)
     after_tr_text = translated.text
     output_str = (
-        f"**Translated By Friday Assistant Bot** \n"
+        f"**Translated By VirtualUserbot Assistant** \n"
         f"Source {translated.src} \nTranslation {lan} \nWhat I Can Translate From This {after_tr_text}"
     )
     try:
