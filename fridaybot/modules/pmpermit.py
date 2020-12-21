a5a005208df5c9bafa36891f17378858a42137ae
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-21 15:05:29 +0530

    Update pmpermit.py

--- fridaybot/modules/pmpermit.py
+++ fridaybot/modules/pmpermit.py
@@ -5,14 +5,14 @@ import os
 from telethon import events, functions
 from telethon.tl.functions.users import GetFullUserRequest
 
-from .sql_helper import pmpermit_sql as pmpermit_sql
+from .sql_helperimport pmpermit_sql as pmpermit_sql
 from fridaybot import ALIVE_NAME, CUSTOM_PMPERMIT
 from fridaybot.Configs import Config
 from fridaybot.utils import friday_on_cmd
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-    WARN_PIC = "https://telegra.ph/file/b1922e21e94ba5a7a19d0.jpg"
+    WARN_PIC = "https://telegra.ph/file/53aed76a90e38779161b1.jpg"
 else:
     WARN_PIC = PMPERMIT_PIC
 
@@ -26,7 +26,6 @@ DEFAULTUSER = (
 )
 CUSTOM_MIDDLE_PMP = (
     str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else f"Protection By {DEFAULTUSER} ❤️"
-)
 USER_BOT_WARN_ZERO = "ඔයා මගේ මාස්ටර් ගෙ Inbox එකට Spam ගහන්න හදපු නිසා මම ඔයාව Block කරා"
 
 botisnoob = Var.TG_BOT_USER_NAME_BF_HER
@@ -60,7 +59,7 @@ if Var.PRIVATE_GROUP_ID is not None:
                 await asyncio.sleep(3)
                 await event.delete()
 
-    @command(pattern=".block$")
+    @borg.on(friday_on_cmd(pattern="block$"))
     async def approve_p_m(event):
         if event.fwd_from:
             return
@@ -70,11 +69,10 @@ if Var.PRIVATE_GROUP_ID is not None:
         if event.is_private:
             if pmpermit_sql.is_approved(chat.id):
                 pmpermit_sql.disapprove(chat.id)
-                await event.edit(
+            await event.edit(
                     "Blocked [{}](tg://user?id={})".format(firstname, chat.id)
                 )
-                await asyncio.sleep(3)
-                await event.client(functions.contacts.BlockRequest(chat.id))
+            await event.client(functions.contacts.BlockRequest(chat.id))
 
     @borg.on(friday_on_cmd(pattern="(da|disapprove)"))
     async def approve_p_m(event):
