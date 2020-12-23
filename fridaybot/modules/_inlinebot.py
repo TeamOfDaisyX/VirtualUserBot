4f480e4b5622cd85a98058453eef5ffeb92e4865
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-23 09:38:29 +0530

    Update _inlinebot.py

--- fridaybot/modules/_inlinebot.py
+++ fridaybot/modules/_inlinebot.py
@@ -226,7 +226,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = sorted(helpable_modules)
     modules = [
         custom.Button.inline(
-            "{} {} {}".format("🔅", x, "🔅"), data="us_plugin_{}".format(x)
+            "{} {} {}".format("🀄️", x, "🀄️"), data="us_plugin_{}".format(x)
         )
         for x in helpable_modules
     ]
