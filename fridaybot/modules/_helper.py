e88dfd841669c67ca42cd934f7308c4c2b0c357f
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-23 08:53:29 +0530

    Update _helper.py

--- fridaybot/modules/_helper.py
+++ fridaybot/modules/_helper.py
@@ -29,8 +29,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """VirtualUserbot Modules Are Listed Here !\n
-For More Help or Support contact Devs @InfinityJE"""
+            help_string = f"""VirtualUserbot Modules Are Listed Here !\n
+For More Help or Support contact {DEFAULTUSER}"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
             )
