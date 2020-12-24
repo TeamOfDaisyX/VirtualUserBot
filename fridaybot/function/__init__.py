625be4d429ec1a4ce11d920722db4f2b263dec3c
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-25 00:00:23 +0530

    Update __init__.py

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -1,1 +1,315 @@
+import asyncio
+import json
+import math
+import os
+import re
+import shlex
+import subprocess
+import time
+from os.path import basename
+from typing import List, Optional, Tuple
+
+import requests
+from bs4 import BeautifulSoup as bs
+from pymediainfo import MediaInfo
+from telethon.tl.types import MessageMediaPhoto
+
+BASE_URL = "https://isubtitles.org"
+from fridaybot.Configs import Config
+
+sedpath = Config.TMP_DOWNLOAD_DIRECTORY
+from fridaybot import logging
+
+logger = logging.getLogger("[--WARNING--]")
+if not os.path.isdir(sedpath):
+    os.makedirs(sedpath)
+
+
+# Thanks To Userge-X
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """ run command in terminal """
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
+
+async def progress(current, total, event, start, type_of_ps, file_name=None):
+    """Generic progress_callback for uploads and downloads."""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current != total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "[{0}{1}] {2}%\n".format(
+            "".join(["▰" for i in range(math.floor(percentage / 10))]),
+            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+        )
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            await event.edit(
+                "{}\nFile Name: `{}`\n{}".format(type_of_ps, file_name, tmp)
+            )
+        else:
+            await event.edit("{}\n{}".format(type_of_ps, tmp))
+
+
+def humanbytes(size):
+    """Input size in bytes,
+    outputs in a human readable format"""
+    # https://stackoverflow.com/a/49361727/4723940
+    if not size:
+        return ""
+    # 2 ** 10 = 1024
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + " day(s), ") if days else "")
+        + ((str(hours) + " hour(s), ") if hours else "")
+        + ((str(minutes) + " minute(s), ") if minutes else "")
+        + ((str(seconds) + " second(s), ") if seconds else "")
+        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+    )
+    return tmp[:-2]
+
+
+# Thanks To Userge-X
+# Ported By @STARKXD
+async def convert_to_image(event, borg):
+    lmao = await event.get_reply_message()
+    if not (
+        lmao.gif
+        or lmao.audio
+        or lmao.voice
+        or lmao.video
+        or lmao.video_note
+        or lmao.photo
+        or lmao.sticker
+    ):
+        await event.edit("`Format Not Supported.`")
+        return
+    else:
+        try:
+            c_time = time.time()
+            downloaded_file_name = await borg.download_media(
+                lmao.media,
+                sedpath,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, event, c_time, "`Downloading...`")
+                ),
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await event.edit(str(e))
+        else:
+            await event.edit(
+                "Downloaded to `{}` successfully.".format(downloaded_file_name)
+            )
+    if not os.path.exists(downloaded_file_name):
+        await event.edit("Download Unsucessfull :(")
+        return
+    if lmao and lmao.photo:
+        lmao_final = downloaded_file_name
+    elif lmao.sticker and lmao.sticker.mime_type == "application/x-tgsticker":
+        rpath = downloaded_file_name
+        image_name20 = os.path.join(sedpath, "SED.png")
+        cmd = f"lottie_convert.py --frame 0 -if lottie -of png {downloaded_file_name} {image_name20}"
+        stdout, stderr = (await runcmd(cmd))[:2]
+        os.remove(rpath)
+        lmao_final = image_name20
+    elif lmao.sticker and lmao.sticker.mime_type == "image/webp":
+        pathofsticker2 = downloaded_file_name
+        image_new_path = sedpath + "image.png"
+        os.rename(pathofsticker2, image_new_path)
+        if not os.path.exists(image_new_path):
+            await event.edit("`Wasn't Able To Fetch Shot.`")
+            return
+        lmao_final = image_new_path
+    elif lmao.audio:
+        sed_p = downloaded_file_name
+        hmmyes = sedpath + "stark.mp3"
+        imgpath = sedpath + "starky.jpg"
+        os.rename(sed_p, hmmyes)
+        await runcmd(f"ffmpeg -i {hmmyes} -filter:v scale=500:500 -an {imgpath}")
+        os.remove(sed_p)
+        if not os.path.exists(imgpath):
+            await event.edit("`Wasn't Able To Fetch Shot.`")
+            return
+        lmao_final = imgpath
+    elif lmao.gif or lmao.video or lmao.video_note:
+        sed_p2 = downloaded_file_name
+        jpg_file = os.path.join(sedpath, "image.jpg")
+        await take_screen_shot(sed_p2, 0, jpg_file)
+        os.remove(sed_p2)
+        if not os.path.exists(jpg_file):
+            await event.edit("`Couldn't Fetch. SS`")
+            return
+        lmao_final = jpg_file
+    await event.edit("`Almost Completed.`")
+    return lmao_final
+
+
+# Thanks To Userge-X
+async def crop_vid(input_vid: str, final_path: str):
+    media_info = MediaInfo.parse(input_vid)
+    for track in media_info.tracks:
+        if track.track_type == "Video":
+            aspect_ratio = track.display_aspect_ratio
+            height = track.height
+            width = track.width
+    if aspect_ratio != 1:
+        crop_by = width if (height > width) else height
+        os.system(f'ffmpeg -i {input_vid} -vf "crop={crop_by}:{crop_by}" {final_path}')
+        os.remove(input_vid)
+    else:
+        os.rename(input_vid, final_path)
+
+
+# Thanks To Userge-X
+async def take_screen_shot(
+    video_file: str, duration: int, path: str = ""
+) -> Optional[str]:
+    """ take a screenshot """
+    logger.info(
+        "[[[Extracting a frame from %s ||| Video duration => %s]]]",
+        video_file,
+        duration,
+    )
+    ttl = duration // 2
+    thumb_image_path = path or os.path.join(sedpath, f"{basename(video_file)}.jpg")
+    command = f'''ffmpeg -ss {ttl} -i "{video_file}" -vframes 1 "{thumb_image_path}"'''
+    err = (await runcmd(command))[1]
+    if err:
+        logger.error(err)
+    return thumb_image_path if os.path.exists(thumb_image_path) else None
+
+
+# Thanks To @HeisenbergTheDanger, @xditya
+async def fetch_feds(event, borg):
+    fedList = []
+    await event.edit("`Fetching Your FeD List`, This May Take A While.")
+    async with borg.conversation("@MissRose_bot") as bot_conv:
+        await bot_conv.send_message("/start")
+        await bot_conv.send_message("/myfeds")
+        await asyncio.sleep(3)
+        response = await bot_conv.get_response(timeout=300)
+        await asyncio.sleep(3)
+        if "You can only use fed commands once every 5 minutes" in response.text:
+            await event.edit("`Try again after 5 mins.`")
+            return
+        elif "make a file" in response.text:
+            await event.edit(
+                "`Boss, You Real Peru. You Are Admin in So Many Feds. WoW!`"
+            )
+            await asyncio.sleep(6)
+            await response.click(0)
+            await asyncio.sleep(6)
+            fedfile = await bot_conv.get_response()
+            await asyncio.sleep(3)
+            if fedfile.media:
+                downloaded_file_name = await borg.download_media(fedfile, "fedlist")
+                await asyncio.sleep(6)
+                file = open(downloaded_file_name, "r")
+                lines = file.readlines()
+                for line in lines:
+                    try:
+                        fedList.append(line[:36])
+                    except BaseException:
+                        pass
+                # CleanUp
+                os.remove(downloaded_file_name)
+        else:
+            In = False
+            tempFedId = ""
+            for x in response.text:
+                if x == "`":
+                    if In:
+                        In = False
+                        fedList.append(tempFedId)
+                        tempFedId = ""
+                    else:
+                        In = True
+
+                elif In:
+                    tempFedId += x
+    await event.edit("`FeD List Fetched SucessFully.`")
+    return fedList
+
+
+async def get_imdb_id(search, event):
+    link = "https://yts-subs.com/search/ajax?mov=" + search
+    lol = requests.get(link)
+    warner_bros = lol.json()
+    if warner_bros == []:
+        await event.edit("`No Results Found.`")
+        warner_media = None
+        warner_s = None
+    else:
+        warner_media = warner_bros[0]["mv_mainTitle"]
+        warner_s = warner_bros[0]["mv_imdbCode"]
+    return warner_media, warner_s
+
+
+async def get_subtitles(imdb_id, borg, event):
+    await event.edit("`Processing..`")
+    link = f"https://yts-subs.com/movie-imdb/" + imdb_id
+    movie_response = requests.get(url=link)
+    subtitles = []
+    soup1 = BeautifulSoup(movie_response.content, "html.parser")
+    rows = soup1.find_all("tr", class_="high-rating")
+    for row in rows:
+        td = row.find("td", class_="flag-cell")
+        lang = td.find("span", class_="sub-lang").text
+        if lang == "English":
+            sub_link_tag = row.find("td", class_="download-cell")
+            sub_link = sub_link_tag.find("a", class_="subtitle-download").get("href")
+            sub_link = f"https://yts-subs.com/{sub_link}"
+            sub_name_tag = row.find("td", class_=None)
+            sub_name = (
+                str(sub_name_tag.find("a").text)
+                .replace("subtitle", "")
+                .replace("\n", "")
+            )
+            sub = (sub_name, sub_link)
+            subtitles.append(sub)
+    await event.edit("`Almost Done.`")
+    sub_response = requests.get(url=subtitles[0]["sub_link"])
+    selected_sub_name = subtitles[0]["sub_name"]
+    soup2 = BeautifulSoup(sub_response.content, "html.parser")
+    link = soup2.find("a", class_="btn-icon download-subtitle").get("href")
+    final_response = requests.get(link, stream=True)
+    await event.edit("`Downloading Now`")
+    if final_response.status_code == 200:
+        with open(sedpath + f"{selected_sub_name}.zip", "wb") as sfile:
+            for byte in final_response.iter_content(chunk_size=128):
+                sfile.write(byte)
+    final_paths = sedpath + f"{selected_sub_name}.zip"
+    namez = selected_sub_name
+    return final_paths, namez, subtitles[0]["sub_link"]
 
