f2fae7c0c2c9688775a71a73fac3e454a1bf5aef
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-31 14:54:07 +0530

    Update carbonRGB.py

--- fridaybot/modules/carbonRGB.py
+++ fridaybot/modules/carbonRGB.py
@@ -102,7 +102,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbonised by [Dark-Cobra](https://github.com/DARK-COBRA/DARKCOBRA)",
+         caption="Carbonised by [VirtualUserbot](https://github.com/inukaasih/virtualuserbot)",
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
