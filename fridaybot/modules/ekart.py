28a9cb011e728089a746455b672149adc59b656e
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-31 23:33:45 +0530

    Create ekart.py

---
+++ fridaybot/modules/ekart.py
@@ -0,0 +1,48 @@
+
+from uniborg.util import friday_on_cmd
+import requests
+from fridaybot import CMD_HELP
+from fridaybot.utils import admin_cmd
+
+
+
+@friday.on(admin_cmd(pattern="ekart (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    urlo = "https://track.aftership.com/trackings?courier=ekart&tracking-numbers=" + str(input_str)
+
+    url = "https://ekart-api-chi.vercel.app/check?id=" + str(input_str)
+    r = requests.get(url)
+    h = r.json()
+    merchant = h.get("merchant_name")
+    order_status = h.get("order_status")
+    kk = h.get("updates")
+    oqwz = kk[0]
+    aq = oqwz.get("Date")
+    ar = oqwz.get("Time")
+    place = oqwz.get("Place")
+    status = oqwz.get("Status")
+
+
+    caption = f""" <b>Ekart Tracking </b>
+Merchant Name:- {merchant}
+Order Status:- {order_status}
+Tracking Id:- {input_str}
+Latest Update
+Date:- {aq}
+Time:- {ar}
+Place:- {place}
+Status:- {status}
+Detailed link:- {urlo}
+<u><b>Ekart Search Completed By Friday.
+Get Your Own Friday From @FRIDAYCHAT.</b></u>
+"""
+    await borg.send_message(
+        event.chat_id,
+        caption,
+        parse_mode="HTML",
+    )
+    await event.delete()
+
