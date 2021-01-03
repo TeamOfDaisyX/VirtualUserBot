068cf1aaace49839507d3b554594f19c7c886362
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2021-01-03 20:37:20 +0530

    Pornhub Search!!

--- fridaybot/modules/nsfw.py
+++ fridaybot/modules/nsfw.py
@@ -16,7 +16,10 @@ import requests
 from telethon.tl.types import MessageMediaPhoto
 
 from fridaybot import CMD_HELP
-from fridaybot.utils import friday_on_cmd, sudo_cmd
+from fridaybot.utils import friday_on_cmd, sudo_cmd, admin_cmd
+from pornhub_api import PornhubApi
+from uniborg.util import friday_on_cmd
+
 
 
 @friday.on(friday_on_cmd(pattern=r"nsfw"))
@@ -49,10 +52,46 @@ async def nsfw(event):
                 os.remove(photo)
 
 
+@friday.on(admin_cmd(pattern="phs (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    api = PornhubApi()
+    data = api.search.search(
+    input_str,
+    ordering="mostviewed"
+    )
+    ok = 1
+    oik = ""
+    for vid in data.videos:
+      if ok<=5:
+        oik +=(f"""
+Video title:- {vid.title}
+Video link:- https://www.pornhub.com/view_video.php?viewkey={vid.video_id}
+        """)
+        ok = ok+1
+      else:
+        pass
+    
+    oiko = "<b>Links Generated Successfully</b>"+"\n"+"Search Query:- "+input_str+"\n"+oik
+    
+    await borg.send_message(
+        event.chat_id,
+        oiko,
+        parse_mode="HTML",
+    )
+    await event.delete()
+
+
+
+
 CMD_HELP.update(
     {
         "nsfw": "**NSFW**\
 \n\n**Syntax : **`.nsfw <reply to image>`\
-\n**Usage :** Checks if the replyed image is nsfw or not."
+\n**Usage :** Checks if the replyed image is nsfw or not.\
+\n\n**Syntax : **`.phs <query>`\
+\n**Usage :** Searches PornHub Website With Given Query."
     }
 )
