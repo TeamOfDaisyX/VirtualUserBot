11a6ddaa56f068bb9e18de3589fe16ae12ec50ff
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2021-01-06 18:43:47 +0530

    Update fedban.py

--- fridaybot/modules/fedban.py
+++ fridaybot/modules/fedban.py
@@ -1,120 +1,57 @@
-from fridaybot.function import fetch_feds
-from fridaybot.modules.sql_helper.feds_sql import (
-    add_fed,
-    get_all_feds,
-    is_fed_indb,
-    rmfed,
+import time
+from datetime import datetime
+
+from fridaybot import CMD_HELP, Lastupdate
+from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+@friday.on(friday_on_cmd(pattern="ping$"))
+@friday.on(sudo_cmd(pattern="ping$", allow_sudo=True))
+async def _(event):
+    starkislub = await edit_or_reply(event, "`Pong !`")
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    uptime = get_readable_time((time.time() - Lastupdate))
+    await starkislub.edit(
+        f"**🙈වැඩ වැඩ මහතයෝ⚙🔧** \n **ᵐʸ ᶜᵘʳʳᵉⁿᵗ ᵖᶦⁿᵍʳᵃᵗᵉ**👇 \n \n ⚡️ `{ms}` \n ⚡️ `{uptime}`"
+    )
+
+
+CMD_HELP.update(
+    {
+        "ping": "**Ping**\
+\n\n**Syntax : **`.pin`\
+\n**Usage :** Get uptime and speed of your bot."
+    }
 )
-from fridaybot.utils import friday_on_cmd
-
-chnnl_grp = Config.FBAN_GROUP
-
-
-@friday.on(friday_on_cmd(pattern="fadd ?(.*)"))
-async def _(event):
-    nolol = 0
-    yeslol = 0
-    await event.edit("`Processing..`")
-    lol_s = event.pattern_match.group(1)
-    if lol_s == None:
-        await event.edit("`Give FeD ID`")
-        return
-    elif lol_s == " ":
-        await event.edit("`Give FeD ID`")
-        return
-    elif lol_s == "all":
-        hmm = await fetch_feds(event, borg)
-        for i in hmm:
-            try:
-                if is_fed_indb(i):
-                    nolol += 1
-                elif not is_fed_indb(i):
-                    add_fed(i)
-                    yeslol += 1
-            except:
-                pass
-        await event.edit(f"`Added {yeslol} Feds To DB, Failed To Add {nolol} Feds.`")
-    elif is_fed_indb(lol_s):
-        await event.edit("`Fed Already Found On DataBase.`")
-        return
-    elif not is_fed_indb(lol_s):
-        add_fed(lol_s)
-        await event.edit("`Done ! Added This Fed To DataBase`")
-
-
-@friday.on(friday_on_cmd(pattern="frm ?(.*)"))
-async def _(event):
-    lol_s = event.pattern_match.group(1)
-    await event.edit("`Processing..`")
-    lol = get_all_feds()
-    if lol_s == None:
-        await event.edit("`Give FeD ID`")
-        return
-    elif lol_s == " ":
-        await event.edit("`Give FeD ID`")
-        return
-    elif lol_s == "all":
-        for sedm in lol:
-            rmfed(sedm.feds)
-        await event.edit("`Done, Cleared. All Fed Database.")
-    elif is_fed_indb(lol_s):
-        rmfed(lol_s)
-        await event.edit("`Done, Removed This FeD From DB`")
-    elif not is_fed_indb(lol_s):
-        await event.edit("`This Fed Not Found On Db.`")
-
-
-@friday.on(friday_on_cmd(pattern="fban ?(.*)"))
-async def _(event):
-    lol_s = event.pattern_match.group(1)
-    if lol_s == None:
-        await event.edit("`No user Found To Fban.`")
-        return
-    all_fed = get_all_feds()
-    errors = 0
-    len_feds = len(all_fed)
-    if len_feds == 0:
-        await event.edit("`No Fed IN DB, Add One To Do So.`")
-        return
-    await event.edit(f"`FBanning in {len_feds} Feds.`")
-    try:
-        await borg.send_message(chnnl_grp, "/start")
-    except Exception as e:
-        await event.edit("**Errors** : " + str(e))
-        return
-    for teamz in all_fed:
-        try:
-            await borg.send_message(chnnl_grp, "/joinfed " + teamz.feds)
-            await borg.send_message(chnnl_grp, "/fban " + lol_s)
-            await asyncio.sleep(5)
-        except:
-            errors += 1
-    await event.edit(
-        f"**Fban Completed** \nTotal Sucess : `{errors - len_feds}` \nTotal Errors : `{errors}` \nTotal Fed Len : `{len_feds}`"
-    )
-
-
-@friday.on(friday_on_cmd(pattern="unfban ?(.*)"))
-async def _(event):
-    lol_s = event.pattern_match.group(1)
-    if lol_s == None:
-        await event.edit("`No user Found To Fban.`")
-        return
-    all_fed = get_all_feds()
-    errors = 0
-    len_feds = len(all_fed)
-    await event.edit(f"`UnFBanning in {len_feds} Feds.`")
-    try:
-        await borg.send_message(chnnl_grp, "/start")
-    except Exception as e:
-        await event.edit("**Errors** : " + str(e))
-        return
-        for teamz in all_fed:
-            try:
-                await borg.send_message(chnnl_grp, "/unfban " + lol_s)
-                await asyncio.sleep(5)
-            except:
-                errors += 1
-    await event.edit(
-        f"**Un-Fban Completed** \nTotal Sucess : `{errors - len_feds}` \nTotal Errors : `{errors}` \nTotal Fed Len : `{len_feds}`"
-    )
