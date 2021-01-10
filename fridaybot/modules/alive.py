95eb16dbbca0d5cca6afb0053ad2130505a18650
SLdevilX <52373359+SLdevilX@users.noreply.github.com>
2021-01-10 13:40:12 +0530

    Update alive.py

--- fridaybot/modules/alive.py
+++ fridaybot/modules/alive.py
@@ -54,8 +54,8 @@ pm_caption += "➣ **RDP**                      `Azureˢˢˡ`\n"
 pm_caption += "➣ **License**                [GNU GPLv3.0](github.com/inukaasith/virtualuserbot/blob/master/LICENSE)\n"
 pm_caption += "➣ **Copyright**             [@virtualuserbot](https://github.com/inukaasith/virtualuserbot)\n"
 pm_caption += "➣ **Check **                  `.stat`. \n\n"
-pm_caption += "[Contact Dev](https://t.me/inukaasith)"
-pm_caption += "[Contact Dev](https://t.me/Zzlll_lllzZ)"
+pm_caption += "[Contact Dev](https://t.me/inukaasith \n)"
+pm_caption += "[Contact Mod](https://t.me/Zzlll_lllzZ)"
 
 @friday.on(friday_on_cmd(pattern=r"alive"))
 @friday.on(sudo_cmd(pattern=r"alive", allow_sudo=True))
