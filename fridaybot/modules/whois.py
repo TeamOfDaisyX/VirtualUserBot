a2d2d359d6a75ec9b1985a37989f9469a7e1aff2
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-11 17:23:33 +0530

    Update whois.py

--- fridaybot/modules/whois.py
+++ fridaybot/modules/whois.py
@@ -194,7 +194,7 @@ async def fetch_info(replied_user, event):
     try:
         dc_id, location = get_input_location(replied_user.profile_photo)
     except:
-        dc_id = "Couldn't fetch DC ID!"
+        dc_id = "Couldn't fetch ViU ID!"
     common_chat = replied_user.common_chats_count
     username = replied_user.user.username
     user_bio = replied_user.about
@@ -214,7 +214,7 @@ async def fetch_info(replied_user, event):
     )
     username = "@{}".format(username) if username else ("This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
-    caption = "<b>USER INFO from DARK COBRA's database :</b>\n\n"
+    caption = "<b>USER INFO fromVirtualUserbot's database :</b>\n\n"
     caption += f"👤First Name: {first_name} {last_name}\n"
     caption += f"🤵Username: {username}\n"
     caption += f"🔖ID: <code>{user_id}</code>\n"
