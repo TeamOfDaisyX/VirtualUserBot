8e7f956d34c68d7ee9965a17ebe3b56461cde3a2
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-16 17:55:14 +0530

    Update qbot.py

--- fridaybot/modules/qbot.py
+++ fridaybot/modules/qbot.py
@@ -8,7 +8,7 @@ from fridaybot import bot
 from ..utils import admin_cmd
 
 
-@borg.on(admin_cmd(pattern=r"qubot(?: |$)(.*)"))
+@borg.on(admin_cmd(pattern=r"qbot(?: |$)(.*)"))
 async def _(event):
     if event.fwd_from:
         return
