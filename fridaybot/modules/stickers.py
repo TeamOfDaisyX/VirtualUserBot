560eca682279caad2ffa0395a6e0d319d702a042
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-18 18:04:44 +0530

    Update stickers.py

--- fridaybot/modules/stickers.py
+++ fridaybot/modules/stickers.py
@@ -201,7 +201,7 @@ async def _(event):
     await moods.edit(
         f"`This Sticker Has Came To Your Pack.` \n**Check It Out** [Here](t.me/addstickers/{packshortname})"
     )
-    os.remove(sedpath + "/" + "@FridayOT.png")
+    os.remove(sedpath + "/" + "@VirtualUserbot.png")
 
 
 @friday.on(friday_on_cmd(pattern="packinfo"))
