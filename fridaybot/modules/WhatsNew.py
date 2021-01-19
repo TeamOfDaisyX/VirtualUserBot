f302e621a9a220743341ade93d980a0850ef4a95
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-19 18:38:08 +0530

    An INFINITY_BOTS Project

--- fridaybot/modules/WhatsNew.py
+++ fridaybot/modules/WhatsNew.py
@@ -2,34 +2,24 @@ from fridaybot import CMD_HELP
 
 CMD_HELP.update(
     {
-        "Whats New": """**Update 3.0 :** VirtualUserbot New features
-        Brings You the Most Stable Reliese ever.
+        "Whats New": """**Mini Update 3.1 :** VirtualUserbot New features
+        Fixed the Errors.
         
-        ** Pro Mode **
-        You'll see some plugins labled as PRO from now on
-        For your protection we limited some adult plugins only to pro's
-        `.set var pro True` => To Be a Pro (It is Free.. Not resposible for Bans)
+        ** Errors and Bug Fixes **
+        No errors anymore.. All logging features running now
         
         ** Custom Emoji on Help **
         `.set var EMOJI_TO_DISPLAY_IN_HELP <emoji>`
-        
-        ** New features as Exclusive Module **
-        See yourself on help as `exclusive`
-        
+               
         ** Stickers Improovements **
         Kang any Media
-        
-        ** Config Funs **
-        See by yourself in Sinhala_Jokes
-        
+              
         ** New qbot **
         `.qbot` to create quotes
         
-        ** 7z Archiver **
-        see by Yourself in archiver
-        
         ** Plus More **
-        New `.mspam` mode (PRO), More ImageTools , Less Spammy now
+        PM Protection done right. Translation also done right
+        
         ** Many Bug Fixes **       
 """
     }
