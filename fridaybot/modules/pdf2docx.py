3c20b2dbcba89040faf9e25d2926cff99e42cf5f
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-22 11:44:15 +0530

    Update pdf2docx.py

--- fridaybot/modules/pdf2docx.py
+++ fridaybot/modules/pdf2docx.py
@@ -1,45 +1,51 @@
+import asyncio
 import os
-
-from pdf2docx import parse
-
+import time
+import time as t
+import zipfile
+from datetime import datetime
+import shutil
 from fridaybot import CMD_HELP
+from fridaybot.function import convert_to_image, crop_vid, runcmd
 from fridaybot.utils import friday_on_cmd, sudo_cmd
+from pdf2docx import parse
+import glob
+import string 
+import random 
+import uuid
+
+
 
 if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-    os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-
-
+  os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+  
 @friday.on(friday_on_cmd(pattern=r"pdf2docx"))
-@friday.on(sudo_cmd(pattern=r"pdf2docx", allow_sudo=True))
 async def hmm(event):
     if not event.reply_to_msg_id:
-        await event.reply("Reply to any Pdf File.")
+        await event.edit("Reply to any Pdf File.")
         return
-    await event.reply("hmm... Please Wait...🚶")
+    hmmu = await event.edit("hmm... Please Wait...🚶")
     lol = await event.get_reply_message()
     starky = await borg.download_media(lol.media, Config.TMP_DOWNLOAD_DIRECTORY)
-
-    await event.reply("hmm... Please Wait...🚶")
-
+    hmmu = await event.edit("hmm... Please Wait..")
     pdf_file = starky
-    docx_file = "./fridaybot/DOWNLOADS/FRIDAYOT.docx"
-
+    docx_file = './fridaybot/DOWNLOADS/FRIDAYOT.docx'
     parse(pdf_file, docx_file, start=0, end=None)
-
     await borg.send_file(
-        event.chat_id,
-        docx_file,
-        caption=f"*PDF Converted Into Docx by VirtualUserbot. Special thanks to @FRIDAYOT.",
+        event.chat_id, docx_file, caption=f"*PDF Converted Into Docx by Friday bot. Get your Friday From @FRIDAYOT."
     )
     os.remove(pdf_file)
     os.remove(docx_file)
     await event.delete()
 
 
+
+
 CMD_HELP.update(
     {
         "fileTools": "**File Tools**\
 \n\n**Syntax : **`.pdf2docx <reply to pdf>`\
-\n**Usage :** Converts Given Pdf Into Docx."
+\n**Usage :** Converts Given Pdf Into Docx.\
+\n\n**Syntax : **`.p2dcl <channel username>`\
+\n**Usage :** Converts All The Pdf's From Channel Into Docx."
     }
-)
