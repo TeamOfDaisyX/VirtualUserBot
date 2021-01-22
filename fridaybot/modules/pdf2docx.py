f169dbfe2e85591d0bcb593000e106a4772f054a
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-22 11:45:47 +0530

    Update pdf2docx.py

--- fridaybot/modules/pdf2docx.py
+++ fridaybot/modules/pdf2docx.py
@@ -1,34 +1,44 @@
+import asyncio
 import os
-
-from pdf2docx import parse
-
+import time
+import time as t
+import zipfile
+from datetime import datetime
+import shutil
 from fridaybot import CMD_HELP
-from fridaybot.utils import friday_on_cmd
+from fridaybot.function import convert_to_image, crop_vid, runcmd
+from fridaybot.utils import friday_on_cmd, sudo_cmd
+from pdf2docx import parse
+import glob
+import string 
+import random 
+import uuid
+
+
 
 if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-    os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-
-
+  os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+  
 @friday.on(friday_on_cmd(pattern=r"pdf2docx"))
 async def hmm(event):
     if not event.reply_to_msg_id:
         await event.edit("Reply to any Pdf File.")
         return
-    await event.edit("hmm... Please Wait...🚶")
+    hmmu = await event.edit("hmm... Please Wait...🚶")
     lol = await event.get_reply_message()
     starky = await borg.download_media(lol.media, Config.TMP_DOWNLOAD_DIRECTORY)
-    await event.edit("hmm... Please Wait..")
+    hmmu = await event.edit("hmm... Please Wait..")
     pdf_file = starky
-    docx_file = "./fridaybot/DOWNLOADS/FRIDAYOT.docx"
+    docx_file = './fridaybot/DOWNLOADS/Infinity_Bots.docx'
     parse(pdf_file, docx_file, start=0, end=None)
     await borg.send_file(
-        event.chat_id,
-        docx_file,
-        caption=f"*PDF Converted Into Docx by Friday bot. Get your Friday From @FRIDAYOT.",
+        event.chat_id, docx_file, caption=f"*PDF Converted Into Docx by VirtualUserbot. Credits to @FRIDAYOT."
     )
     os.remove(pdf_file)
     os.remove(docx_file)
     await event.delete()
+
+
 
 
 CMD_HELP.update(
