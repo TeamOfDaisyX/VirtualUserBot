64b80d766511e9f9af5ce1ce727976ae8e77847c
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-26 23:39:26 +0530

    Update gtools.py

--- virtualuserbot/modules/gtools.py
+++ virtualuserbot/modules/gtools.py
@@ -1,18 +1,145 @@
+#    Copyright (C) Midhun KM 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import asyncio
 
 from telethon.events import ChatAction
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
 from telethon.tl.types import MessageEntityMentionName
-
 from virtualuserbot import CMD_HELP
 from virtualuserbot.modules.sql_helper.mute_sql import is_muted, mute, unmute
 from virtualuserbot.utils import friday_on_cmd
+from virtualuserbot.function import get_all_admin_chats, is_admin
+from virtualuserbot.modules.sql_helper import gban_sql
+from telethon.events import ChatAction, NewMessage
+from telethon.tl.types import (
+    ChannelParticipantsAdmins,
+    ChatAdminRights,
+    ChatBannedRights,
+    MessageEntityMentionName,
+    MessageMediaPhoto,
+)
+from telethon.errors import (
+    BadRequestError,
+    ChatAdminRequiredError,
+    ImageProcessFailedError,
+    PhotoCropSizeSmallError,
+    UserAdminInvalidError,
+)
+from telethon.errors.rpcerrorlist import MessageTooLongError, UserIdInvalidError
+from telethon.tl.functions.channels import (
+    EditAdminRequest,
+    EditBannedRequest,
+    EditPhotoRequest,
+)
 
 
-async def get_full_user(event):
-    args = event.pattern_match.group(1).split(":", 1)
+@friday.on(friday_on_cmd(pattern='gban(?: |$)(.*)'))
+async def gbun(event):
+    await event.edit("**GBanning User**")
+    sucess = 0
+    bad = 0
+    user, reason = await get_user_from_event(event)
+    if not user.id:
+        await event.edit("`Mention A User To Gban`")
+        return
+    if not reason:
+        hmm_r = "#GBanned"
+    elif reason:
+        hmm_r = reason
+    if user.id == bot.uid:
+        await event.edit("**I Can't Gban You Master :(**")
+        return
+    if gban_sql.is_gbanned(user.id):
+        await event.edit("**This User Is Already Gbanned. No Point In Gbanning Him Again !**")
+        return
+    gban_sql.gban_user(user.id, hmm_r)
+    chat_s = await get_all_admin_chats(event)
+    if len(chat_s) == 0:
+        await event.edit("**You Need To Be Admin In Atleast 1 Group To Perform this Action**")
+        return
+    len_s = len(chat_s)
+    await event.edit(f"**GBanning !** [{user.first_name}](tg://user?id={user.id}) **in {len_s} Chats!**")
+    for stark_s in chat_s:
+        try:
+          await event.client.edit_permissions(stark_s, user.id, view_messages=False)
+          sucess += 1
+        except:
+          bad += 0
+    await event.edit(f"**GBanned !**[{user.first_name}](tg://user?id={user.id}) **in {sucess} Chats!**")
+    
+          	
+@friday.on(friday_on_cmd(pattern='ungban(?: |$)(.*)'))
+async def ungbun(event):
+    await event.edit("**Un-GBanning User**")
+    sucess = 0
+    bad = 0
+    user, reason = await get_user_from_event(event)
+    if not user.id:
+        await event.edit("`Mention A User To Un-Gban`")
+        return
+    if user.id == bot.uid:
+        await event.edit("**I Can't Un-Gban You Master :(**")
+        return
+    if not gban_sql.is_gbanned(user.id):
+        await event.edit("**This User Is Not Gbanned. No Point In Un-Gbanning !**")
+        return
+    gban_sql.ungban_user(user.id)
+    chat_s = await get_all_admin_chats(event)
+    if len(chat_s) == 0:
+        await event.edit("**You Need To Be Admin In Atleast 1 Group To Perform this Action**")
+        return
+    len_s = len(chat_s)
+    await event.edit(f"**Un-GBanning !** [{user.first_name}](tg://user?id={user.id}) **in {len_s} Chats!**")
+    for stark_s in chat_s:
+        try:
+          await event.client.edit_permissions(stark_s, user.id, view_messages=True)
+          sucess += 1
+        except:
+          bad += 0
+    await event.edit(f"**Un-GBanned !**[{user.first_name}](tg://user?id={user.id}) **in {sucess} Chats!**")
+
+@friday.on(ChatAction)
+async def starky(event):
+    if event.user_joined:
+        hmm = await bot.get_me()
+        if await is_admin(event, hmm.id):
+            sadly = await event.get_user()
+            if gban_sql.is_gbanned(sadly.id):
+                try:
+                    await event.client.edit_permissions(event.chat_id, sadly.id, view_messages=False)
+                    await event.reply(f"**#GBanned-User** \nUserID : {sadly.id} \nReason : {gban_sql.is_gbanned(sadly.id)}")
+                except:
+                    pass
+                
+@friday.on(NewMessage)
+async def mi(event):
+    sed = event.sender_id
+    hmm = await bot.get_me()
+    if event.is_private:
+        return
+    if await is_admin(event, hmm.id):
+        if gban_sql.is_gbanned(sed):
+            try:
+                await event.client.edit_permissions(event.chat_id, sed, view_messages=False)
+            except:
+                pass
+            
+async def get_user_from_event(event):
+    """ Get the user from argument or replied message. """
+    args = event.pattern_match.group(1).split(" ", 1)
     extra = None
-    if event.reply_to_msg_id and not len(args) == 2:
+    if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         user_obj = await event.client.get_entity(previous_message.sender_id)
         extra = event.pattern_match.group(1)
@@ -20,198 +147,50 @@ async def get_full_user(event):
         user = args[0]
         if len(args) == 2:
             extra = args[1]
+
         if user.isnumeric():
             user = int(user)
+
         if not user:
-            await event.edit("`User ID Is Required")
+            await event.edit("`Pass the user's username, id or reply!`")
             return
+
         if event.message.entities is not None:
             probable_user_mention_entity = event.message.entities[0]
+
             if isinstance(probable_user_mention_entity, MessageEntityMentionName):
                 user_id = probable_user_mention_entity.user_id
                 user_obj = await event.client.get_entity(user_id)
                 return user_obj
+        
         try:
             user_obj = await event.client.get_entity(user)
-        except Exception as err:
-            return await event.edit("Something Went Wrong", str(err))
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    elif event.is_private:
+        hmm = await event.get_input_chat()
+        try:
+            user_obj = await event.client.get_entity(hmm)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+        extra = event.pattern_match.group(1)
     return user_obj, extra
 
 
 async def get_user_sender_id(user, event):
     if isinstance(user, str):
         user = int(user)
+
     try:
         user_obj = await event.client.get_entity(user)
     except (TypeError, ValueError) as err:
         await event.edit(str(err))
         return None
+
     return user_obj
 
-
-@friday.on(friday_on_cmd(pattern="gban ?(.*)"))
-async def gspider(virtualuserbot):
-    lol = virtualuserbot
-    sender = await lol.get_sender()
-    me = await lol.client.get_me()
-    if not sender.id == me.id:
-        friday = await lol.reply("Gbanning This User !")
-    else:
-        friday = await lol.edit("Wait Processing.....")
-    me = await virtualuserbot.client.get_me()
-    await friday.edit(f"Global Ban Is Coming ! Wait And Watch You Nigga")
-    my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
-    f"@{me.username}" if me.username else my_mention
-    await virtualuserbot.get_chat()
-    a = b = 0
-    if virtualuserbot.is_private:
-        user = virtualuserbot.chat
-        reason = virtualuserbot.pattern_match.group(1)
-    else:
-        virtualuserbot.chat.title
-    try:
-        user, reason = await get_full_user(virtualuserbot)
-    except:
-        pass
-    try:
-        if not reason:
-            reason = "Private"
-    except:
-        return await friday.edit(f"**Something W3NT Wrong 🤔**")
-    if user:
-        if user.id == 1263617196:
-            return await friday.edit(
-                f"**Didn't , Your Father Teach You ? That You Cant Gban Dev**"
-            )
-        try:
-            from virtualuserbot.modules.sql_helper.gmute_sql import gmute
-        except:
-            pass
-        try:
-            await virtualuserbot.client(BlockRequest(user))
-        except:
-            pass
-        testvirtualuserbot = [
-            d.entity.id
-            for d in await virtualuserbot.client.get_dialogs()
-            if (d.is_group or d.is_channel)
-        ]
-        for i in testvirtualuserbot:
-            try:
-                await virtualuserbot.client.edit_permissions(
-                    i, user, view_messages=False
-                )
-                a += 1
-                await friday.edit(f"**GBANNED // Total Affected Chats **: `{a}`")
-            except:
-                b += 1
-    else:
-        await friday.edit(f"**Reply to a user !!**")
-    try:
-        if gmute(user.id) is False:
-            return await friday.edit(f"**Error! User probably already gbanned.**")
-    except:
-        pass
-    return await friday.edit(
-        f"**Gbanned [{user.first_name}](tg://user?id={user.id}) Affected Chats : {a} **"
-    )
-
-
-@friday.on(friday_on_cmd(pattern="ungban ?(.*)"))
-async def gspider(virtualuserbot):
-    lol = virtualuserbot
-    sender = await lol.get_sender()
-    me = await lol.client.get_me()
-    if not sender.id == me.id:
-        friday = await lol.reply("`Wait Let Me Process`")
-    else:
-        friday = await lol.edit("One Min ! ")
-    me = await virtualuserbot.client.get_me()
-    await friday.edit(f"Trying To Ungban User !")
-    my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
-    f"@{me.username}" if me.username else my_mention
-    await virtualuserbot.get_chat()
-    a = b = 0
-    if virtualuserbot.is_private:
-        user = virtualuserbot.chat
-        reason = virtualuserbot.pattern_match.group(1)
-    else:
-        virtualuserbot.chat.title
-    try:
-        user, reason = await get_full_user(virtualuserbot)
-    except:
-        pass
-    try:
-        if not reason:
-            reason = "Private"
-    except:
-        return await friday.edit("Someting Went Wrong 🤔")
-    if user:
-        if user.id == 1263617196:
-            return await friday.edit("**You Cant Ungban A Dev !**")
-        try:
-            from virtualuserbot.modules.sql_helper.gmute_sql import ungmute
-        except:
-            pass
-        try:
-            await virtualuserbot.client(UnblockRequest(user))
-        except:
-            pass
-        testvirtualuserbot = [
-            d.entity.id
-            for d in await virtualuserbot.client.get_dialogs()
-            if (d.is_group or d.is_channel)
-        ]
-        for i in testvirtualuserbot:
-            try:
-                await virtualuserbot.client.edit_permissions(
-                    i, user, send_messages=True
-                )
-                a += 1
-                await friday.edit(f"**UNGBANNING // AFFECTED CHATS - {a} **")
-            except:
-                b += 1
-    else:
-        await friday.edit("**Reply to a user !!**")
-    try:
-        if ungmute(user.id) is False:
-            return await friday.edit("**Error! User probably already ungbanned.**")
-    except:
-        pass
-    return await friday.edit(
-        f"**UNGBANNED // USER - [{user.first_name}](tg://user?id={user.id}) CHATS : {a} **"
-    )
-
-
-@friday.on(ChatAction)
-async def handler(rkG):
-    if rkG.user_joined or rkG.user_added:
-        try:
-            from virtualuserbot.modules.sql_helper.gmute_sql import is_gmuted
-
-            guser = await rkG.get_user()
-            gmuted = is_gmuted(guser.id)
-        except:
-            return
-        if gmuted:
-            for i in gmuted:
-                if i.sender == str(guser.id):
-                    chat = await rkG.get_chat()
-                    admin = chat.admin_rights
-                    creator = chat.creator
-                    if admin or creator:
-                        try:
-                            await client.edit_permissions(
-                                rkG.chat_id, guser.id, view_messages=False
-                            )
-                            await rkG.reply(
-                                f"**Gbanned User Joined!!** \n"
-                                f"**Victim Id**: [{guser.id}](tg://user?id={guser.id})\n"
-                                f"**Action **  : `Banned`"
-                            )
-                        except:
-                            rkG.reply("`No Permission To Ban`")
-                            return
 
 
 @friday.on(friday_on_cmd(pattern=r"gmute ?(\d+)?"))
