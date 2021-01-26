6a5e097b8750d0243b30383ba21170e2d0b0598e
InukaAsith <inukaasith7@gmail.com>
2021-01-26 18:18:14 +0000

    pylint: auto fixes

--- virtualuserbot/modules/fbdl.py
+++ virtualuserbot/modules/fbdl.py
@@ -1,10 +1,11 @@
-from datetime import datetime
-import requests
-import re
-import html
 import os
-from virtualuserbot import CMD_HELP, sclient
-from virtualuserbot.utils import edit_or_reply, friday_on_cmd, sudo_cmd, admin_cmd
+import re
+from datetime import datetime
+
+import requests
+
+from virtualuserbot import CMD_HELP
+from virtualuserbot.utils import admin_cmd
 
 fridaythumb = "./resources/IMG_20200929_103719_628.jpg"
 
@@ -12,17 +13,17 @@ fridaythumb = "./resources/IMG_20200929_103719_628.jpg"
 def main(url, filename):
     try:
         download_video("HD", url, filename)
-    except(KeyboardInterrupt):
+    except (KeyboardInterrupt):
         download_video("SD", url, filename)
 
 
 def download_video(quality, url, filename):
-    html = requests.get(url).content.decode('utf-8')
+    html = requests.get(url).content.decode("utf-8")
     video_url = re.search(rf'{quality.lower()}_src:"(.+?)"', html).group(1)
     file_size_request = requests.get(video_url, stream=True)
-    file_size = int(file_size_request.headers['Content-Length'])
+    int(file_size_request.headers["Content-Length"])
     block_size = 1024
-    with open(filename + '.mp4', 'wb') as f:
+    with open(filename + ".mp4", "wb") as f:
         for data in file_size_request.iter_content(block_size):
             f.write(data)
     print("\nVideo downloaded successfully.")
@@ -33,40 +34,42 @@ async def _(event):
     if event.fwd_from:
         return
     url = event.pattern_match.group(1)
-    x = re.match(r'^(https:|)[/][/]www.([^/]+[.])*facebook.com', url)
+    x = re.match(r"^(https:|)[/][/]www.([^/]+[.])*facebook.com", url)
 
     if x:
-        html = requests.get(url).content.decode('utf-8')
+        html = requests.get(url).content.decode("utf-8")
     else:
         await event.edit("This Video Is Either Private Or URL Is Invalid. Exiting... ")
         return
 
     _qualityhd = re.search('hd_src:"https', html)
     _qualitysd = re.search('sd_src:"https', html)
-    _hd = re.search('hd_src:null', html)
-    _sd = re.search('sd_src:null', html)
+    _hd = re.search("hd_src:null", html)
+    _sd = re.search("sd_src:null", html)
 
     list = []
     _thelist = [_qualityhd, _qualitysd, _hd, _sd]
-    for id,val in enumerate(_thelist):
+    for id, val in enumerate(_thelist):
         if val != None:
             list.append(id)
-    filename = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+    filename = datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M-%S")
 
     main(url, filename)
     await event.edit("Video Downloaded Successfully. Starting To Upload.")
-        
+
     kk = f"{filename}.mp4"
-    caption= f"Facebook Video Successfully by Fridaybot.\nGet Your Fridaybot From @Fridayot."
-        
+    caption = (
+        f"Facebook Video Successfully by Fridaybot.\nGet Your Fridaybot From @Fridayot."
+    )
+
     await borg.send_message(
-      event.chat_id,
-      caption,
-      thumb=fridaythumb,
-      parse_mode="HTML",
-      file=kk,
-      force_document=True,
-      allow_cache=False,
+        event.chat_id,
+        caption,
+        thumb=fridaythumb,
+        parse_mode="HTML",
+        file=kk,
+        force_document=True,
+        allow_cache=False,
     )
     os.system(f"rm {kk}")
 
