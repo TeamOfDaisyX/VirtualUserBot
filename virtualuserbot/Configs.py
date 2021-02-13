f0647b9d176f6e255fadd2c36ca27fb2c1788d15
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-13 20:44:13 +0530

    Update Configs.py

--- virtualuserbot/Configs.py
+++ virtualuserbot/Configs.py
@@ -162,7 +162,7 @@ if ENV:
         GPLINKS_API_KEY = os.environ.get("GPLINKS_API_KEY", None)
         OPEN_LOAD_KEY = os.environ.get("OPEN_LOAD_KEY", None)
         UPSTREAM_REPO = os.environ.get(
-            "UPSTREAM_REPO", "https://github.com/Inukaasith/VirtualUserbot"
+            "UPSTREAM_REPO", "https://github.com/DevsExpo/FridayUserbot"
         )
         ALIVE_IMAGE = os.environ.get(
             "ALIVE_IMAGE", "https://telegra.ph/file/4e1364fb18f899ad47dec.png"
