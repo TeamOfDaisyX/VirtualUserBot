34873c887ef33c6f2f8df1cd7a490b769007bc1c
InukaAsith <inukaasith7@gmail.com>
2021-03-31 14:16:44 +0000

    pylint: auto fixes

--- virtualbotmain.py
+++ virtualbotmain.py
@@ -1,3 +1,6 @@
-#To solbe suspension issue #By sh1vam
+# To solbe suspension issue #By sh1vam
 import os
-os.system("git clone https://github.com/InukaAsith/VirtualUserbot /root/virtualuserbot && mkdir /root/virtualuserbot/bin/ && cd /root/virtualuserbot/ && chmod +x /usr/local/bin/* && python3 -m virtualuserbot")
+
+os.system(
+    "git clone https://github.com/InukaAsith/VirtualUserbot /root/virtualuserbot && mkdir /root/virtualuserbot/bin/ && cd /root/virtualuserbot/ && chmod +x /usr/local/bin/* && python3 -m virtualuserbot"
+)
