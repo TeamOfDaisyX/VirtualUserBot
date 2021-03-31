b5476dbda53fced3804c0f33b915c0350f50b0cb
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-31 19:45:59 +0530

    Heroku error fixed

---
+++ virtualbotmain.py
@@ -0,0 +1,3 @@
+#To solbe suspension issue #By sh1vam
+import os
+os.system("git clone https://github.com/InukaAsith/VirtualUserbot /root/virtualuserbot && mkdir /root/virtualuserbot/bin/ && cd /root/virtualuserbot/ && chmod +x /usr/local/bin/* && python3 -m virtualuserbot")
