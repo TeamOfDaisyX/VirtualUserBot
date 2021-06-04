f6c15c585eb829afaea952f0a7b80cc75fd95fdc
Starkgangs <Starkgangz@gmail.com>
2021-06-04 13:34:25 +0000

    pylint: auto fixes

--- fridaybot/modules/pmpermit.py
+++ fridaybot/modules/pmpermit.py
@@ -27,7 +27,9 @@ DEFAULTUSER = (
 CUSTOM_MIDDLE_PMP = (
     str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "Protection By @InukaASiTH ❤️"
 )
-USER_BOT_WARN_ZERO = "ඔයා මගේ මාස්ටර් ගෙ Inbox එකට Spam ගහන්න හදපු නිසා මම ඔයාව Block කරා"
+USER_BOT_WARN_ZERO = (
+    "ඔයා මගේ මාස්ටර් ගෙ Inbox එකට Spam ගහන්න හදපු නිසා මම ඔයාව Block කරා"
+)
 
 botisnoob = Var.TG_BOT_USER_NAME_BF_HER
 
@@ -55,7 +57,9 @@ if Var.PRIVATE_GROUP_ID is not None:
                     del PREV_REPLY_MESSAGE[chats.id]
                 pmpermit_sql.approve(chats.id, "Approved Another Nibba")
                 await event.edit(
-                    "ඔයාව ප්‍රයිවට් මැසේජ් යවන්න Approove උනා [{}](tg://user?id={})".format(firstname, chats.id)
+                    "ඔයාව ප්‍රයිවට් මැසේජ් යවන්න Approove උනා [{}](tg://user?id={})".format(
+                        firstname, chats.id
+                    )
                 )
                 await asyncio.sleep(3)
                 await event.delete()
